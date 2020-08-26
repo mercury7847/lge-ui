3261d616e53ed63490ef6519a7a5aad970ea2fb3
Yun <>
2020-08-26 13:34:03 +0900

    KRP0013, KRC0017

--- src/js/components/KRC0017.js
+++ src/js/components/KRC0017.js
@@ -2,6 +2,8 @@ $(window).ready(function(){
     vcui.require(['ui/carousel'], function () {
         $('.ui_carousel_slider').vcCarousel({
             infinite: false,
+            prevArrow:'.btn-arrow.prev',
+            nextArrow:'.btn-arrow.next',
             swipeToSlide: true,
             slidesToShow: 6,
             slidesToScroll: 1,
