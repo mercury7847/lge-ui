beb6272d77b2f0c684d6c1e14c553e411aba2a99
pparks <pparks@pparksui-MacBookPro.local>
2020-08-26 10:16:51 +0900

    krc0013

--- src/js/ui/youtubeBox.js
+++ src/js/ui/youtubeBox.js
@@ -5,7 +5,11 @@ define('ui/youtubeBox', ['jquery', 'vcui'], function ($, core) {
     var YoutubeBox = core.ui('youtubeBox', {
         bindjQuery: true,
         defaults: {
+            templateUrl: "/html/components/modal-template.html"
         },
+        selectors: { 
+            template: $('<div class="template"></div>')
+        }, 
 
         initialize: function initialize(el, options) {
             var self = this;
@@ -13,14 +17,59 @@ define('ui/youtubeBox', ['jquery', 'vcui'], function ($, core) {
             if (self.supr(el, options) === false) {
                 return;
             };
+
+            self.$modal = null;
+
+            self.templateLoaded = false;
+
+            self.$opener = self.$el.find('a.see-video').css('display', 'block');
+            self.$youtubeURL = self.$opener.attr("data-src");
             
-            self._templateLoad();
+            self.$template.load( self.options.templateUrl, function(html){
+                self._completeTemplate(html)
+            });
+
+            self._bindEvent();
         },
 
-        _templateLoad: function(){
+        _completeTemplate: function(html){
             var self = this;
 
-            
+            self.templateLoaded = true;
+            $('head').append($(html));
+        },
+
+        _bindEvent: function(){
+            var self = this;
+
+            self.$opener.on('click', function(e){
+                e.preventDefault();
+                
+                if(self.templateLoaded) self._addModal();
+            })
+        },
+
+        _addModal: function(){
+            var self = this;
+
+            var modal = vcui.template($('#youtube-full-modal').html(), {youtube_url:self.$youtubeURL});
+            self.$modal = $(modal).get(0);
+            $(self.$modal).find(".close-video").on('click', function(e){
+                e.preventDefault();
+                
+                self._removeModal();
+            })
+
+            $('body').addClass('modal-open').append(self.$modal);
+        },
+
+        _removeModal: function(){
+            var self = this;
+
+            $(self.$modal).remove();
+            self.$modal = null;
+
+            $('body').removeClass('modal-open');
         }
     });
 
