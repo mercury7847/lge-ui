ff19d47f7243d8259a26967d78c42273b3af0a3f
전호근/(협력사) 과장/디지털플랫폼팀(hogeun.jeon@lge.com) <hogeun.jeon@lge.com>
2020-08-26 18:07:32 +0900

    gulpfile, KR50CS411

--- src/js/support-common.js
+++ src/js/support-common.js
@@ -1,30 +1,6 @@
-$(window).ready(function(){
+$(document).ready(function() { 
     
-    vcui.require(['ui/carousel', 'ui/modal'], function () {
-        $('.category-select-carousel').vcCarousel({
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            dots: true,
-            arrows: true,
-            customPaging: function(carousel, i) {
-                var $button = $('<button type="button"><em class="blind">'+(i+1)+'</em></button>');
-                return $button;
-            },
-            responsive: [
-                {
-                    breakpoint:768,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                        dots: true,
-                        arrows: false
-                    }
-                }
-            ]
-        });
-    });
-
-})
+});
 
 // $(window).resize(function() {
 //     if($(window).width() < 768){
