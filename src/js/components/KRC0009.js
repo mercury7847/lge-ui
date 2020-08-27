4ffe4d59f97ba28eb55bf3f2d8abc2410c3ff1c3
pparks <pparks@pparksui-MacBookPro.local>
2020-08-27 17:24:51 +0900

    js 수정

--- src/js/components/KRC0009.js
+++ src/js/components/KRC0009.js
@@ -1,22 +1,25 @@
 $(window).ready(function(){
-    var componentID = "KRC0009";
-
-    if(!document.querySelector('.' + componentID)) return false;
+    if(!document.querySelector('.KRC0009')) return false;
     
-    vcui.require(['ui/carousel'], function () {
-        $('.' + componentID).each(function(idx, item){
-            $(item).find('.ui_carousel_slider').vcCarousel({
-                infinite: false,
-                swipeToSlide: true,
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                responsive: [{
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 1
-                    }
-                }]
+    var KRC0009 = {
+        init: function(){
+            vcui.require(['ui/carousel'], function () {
+                $('.KRC0009').each(function(idx, item){
+                    $(item).find('.ui_carousel_slider').vcCarousel({
+                        infinite: false,
+                        swipeToSlide: true,
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        responsive: [{
+                            breakpoint: 768,
+                            settings: {
+                                slidesToShow: 1
+                            }
+                        }]
+                    });
+                });
             });
-        });
-    });
+        }
+    }
+    KRC0009.init();
 })
--- src/js/components/KRP0013.js
+++ src/js/components/KRP0013.js
@@ -1,51 +1,54 @@
 $(window).ready(function(){
-    var componentID = "KRP0013";
+    if(!document.querySelector('.KRP0013')) return false;
 
-    if(!document.querySelector('.' + componentID)) return false;
-    
-    vcui.require(['ui/carousel'], function () {
-        $('.' + componentID).each(function(idx, item){
-            $(item).find(".ui_carousel_slider").each(function(cdx, slide){
-                if($(slide).hasClass("rowColumn")){
-                    $(slide).vcCarousel({
-                        infinite: false,
-                        swipeToSlide: true,
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        responsive: [
-                            {
-                                breakpoint: 768,
-                                settings: {
-                                    slidesToShow: 1
-                                }
-                            }
-                        ]
-                    })
-                } else{
-                    $(slide).vcCarousel({
-                        infinite: false,
-                        prevArrow:'.btn-arrow.prev',
-                        nextArrow:'.btn-arrow.next',
-                        swipeToSlide: true,
-                        slidesToShow: 4,
-                        slidesToScroll: 1,
-                        responsive: [
-                            {
-                                breakpoint: 1200,
-                                settings: {
-                                    slidesToShow: 2
-                                }
-                            }, 
-                            {
-                                breakpoint: 768,
-                                settings: {
-                                    slidesToShow: 1
-                                }
-                            }
-                        ]
+    var KRP0013 = {
+        init: function(){
+            vcui.require(['ui/carousel'], function () {
+                $('.KRP0013').each(function(idx, item){
+                    $(item).find(".ui_carousel_slider").each(function(cdx, slide){
+                        if($(slide).hasClass("rowColumn")){
+                            $(slide).vcCarousel({
+                                infinite: false,
+                                swipeToSlide: true,
+                                slidesToShow: 2,
+                                slidesToScroll: 1,
+                                responsive: [
+                                    {
+                                        breakpoint: 768,
+                                        settings: {
+                                            slidesToShow: 1
+                                        }
+                                    }
+                                ]
+                            })
+                        } else{
+                            $(slide).vcCarousel({
+                                infinite: false,
+                                prevArrow:'.btn-arrow.prev',
+                                nextArrow:'.btn-arrow.next',
+                                swipeToSlide: true,
+                                slidesToShow: 4,
+                                slidesToScroll: 1,
+                                responsive: [
+                                    {
+                                        breakpoint: 1200,
+                                        settings: {
+                                            slidesToShow: 2
+                                        }
+                                    }, 
+                                    {
+                                        breakpoint: 768,
+                                        settings: {
+                                            slidesToShow: 1
+                                        }
+                                    }
+                                ]
+                            });
+                        }
                     });
-                }
+                });
             });
-        });
-    });
+        }
+    }
+    KRP0013.init();
 })
