2d56208a2f406b87aad37757d5ef49fe308ee6f5
pparks <pparks@pparksui-MacBookPro.local>
2020-08-27 10:19:03 +0900

    KRP0013

--- src/js/components/KRP0013.js
+++ src/js/components/KRP0013.js
@@ -1,42 +1,51 @@
 $(window).ready(function(){
-    vcui.require(['ui/carousel'], function () {
-        $('.ui_carousel_slider').vcCarousel({
-            infinite: false,
-            prevArrow:'.btn-arrow.prev',
-            nextArrow:'.btn-arrow.next',
-            swipeToSlide: true,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            responsive: [
-                {
-                    breakpoint: 1200,
-                    settings: {
-                        slidesToShow: 2
-                    }
-                }, 
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 1
-                    }
-                }
-            ]
-        });
+    var componentID = "KRP0013";
 
-        $('.ui_carousel_slider2').vcCarousel({
-            infinite: false,
-            swipeToSlide: true,
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 1
-                    }
+    if(!document.querySelector('.' + componentID)) return false;
+    
+    vcui.require(['ui/carousel'], function () {
+        $('.' + componentID).each(function(idx, item){
+            $(item).find(".ui_carousel_slider").each(function(cdx, slide){
+                if($(slide).hasClass("rowColumn")){
+                    $(slide).vcCarousel({
+                        infinite: false,
+                        swipeToSlide: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        responsive: [
+                            {
+                                breakpoint: 768,
+                                settings: {
+                                    slidesToShow: 1
+                                }
+                            }
+                        ]
+                    })
+                } else{
+                    $(slide).vcCarousel({
+                        infinite: false,
+                        prevArrow:'.btn-arrow.prev',
+                        nextArrow:'.btn-arrow.next',
+                        swipeToSlide: true,
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                        responsive: [
+                            {
+                                breakpoint: 1200,
+                                settings: {
+                                    slidesToShow: 2
+                                }
+                            }, 
+                            {
+                                breakpoint: 768,
+                                settings: {
+                                    slidesToShow: 1
+                                }
+                            }
+                        ]
+                    });
                 }
-            ]
+            });
         });
     });
 })
-
