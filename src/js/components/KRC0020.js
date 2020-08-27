46ba83f76f9b3590c3bf06a91e18fa3fc181bc8d
pparks <pparks@pparksui-MacBookPro.local>
2020-08-27 18:17:52 +0900

    KRC0020

--- src/js/components/KRC0020.js
+++ src/js/components/KRC0020.js
@@ -1,45 +1,15 @@
 $(window).ready(function(){
     if(!document.querySelector('.KRC0020')) return false;
 
-    var $targetDrag = $('.KRC0020 .drag-area');
-    $targetDrag.each(function(){
-        var dragTitle = $(this).data('handle-title');
-        $(this).find('.twentytwenty-handle').attr('title',dragTitle);
-    });
-
-    var $obj = $('.KRC0020');
-    var waNumber = 0;
-    $obj.each(function() {
-        var $target;
-        if($(this).find('.title') && !$(this).find('.title').is(':empty')) {
-            $target = $(this).find('.title');
-        }
-        if($target) {
-            $target.attr('id', 'waKRC0020_'+waNumber);
-            $(this).find('a.btn').attr('aria-describedby', 'waKRC0020_'+waNumber);
-            $(this).find('a.link-text').attr('aria-describedby', 'waKRC0020_'+waNumber);
-            waNumber++;
-        }
-    });
-});
-
-$(window).load(function(){
-    $('.KRC0020 .drag-area').twentytwenty({
-        no_overlay: true
-    });
-});
-$(window).ready(function(){
-    if(!document.querySelector('.KRC0019')) return false;
-
-    var KRC0019 = {
+    var KRC0020 = {
         init: function(){
-            var $targetDrag = $('.KRC0019 .drag-area');
+            var $targetDrag = $('.KRC0020 .drag-area');
             $targetDrag.each(function(){
                 var dragTitle = $(this).data('handle-title');
                 $(this).find('.twentytwenty-handle').attr('title',dragTitle);
             });
         
-            var $obj = $('.KRC0019');
+            var $obj = $('.KRC0020');
             var waNumber = 0;
             $obj.each(function() {
                 var $target;
@@ -47,19 +17,19 @@ $(window).ready(function(){
                     $target = $(this).find('.title');
                 }
                 if($target) {
-                    $target.attr('id', 'waKRC0019_'+waNumber);
-                    $(this).find('a.btn').attr('aria-describedby', 'waKRC0019_'+waNumber);
-                    $(this).find('a.link-text').attr('aria-describedby', 'waKRC0019_'+waNumber);
+                    $target.attr('id', 'waKRC0020_'+waNumber);
+                    $(this).find('a.btn').attr('aria-describedby', 'waKRC0020_'+waNumber);
+                    $(this).find('a.link-text').attr('aria-describedby', 'waKRC0020_'+waNumber);
                     waNumber++;
                 }
             });
         }
     }
-    KRC0019.init();
+    KRC0020.init();
 });
 
 $(window).load(function(){
-    $('.KRC0019 .drag-area').twentytwenty({
+    $('.KRC0020 .drag-area').twentytwenty({
         no_overlay: true
     });
 });
--- src/js/vcui.common-ui.js
+++ src/js/vcui.common-ui.js
@@ -6701,9 +6701,9 @@ define('ui/tab', ['jquery', 'vcui', 'ui/smoothScroll'], function ($, core) {
         defaults: {
             selectedIndex: 0,
             selectedClass: selectedClass,
-            allowHScroll: false,
             selectedText: '선택됨',
-            tabsSelector: '>ul>li'
+            tabsSelector: '>ul>li',
+            tabForceHeight: false
         },
 
         selectors: {},
@@ -6721,10 +6721,11 @@ define('ui/tab', ['jquery', 'vcui', 'ui/smoothScroll'], function ($, core) {
                 return;
             }
 
-            self.$srText = $('<em class="blind">' + self.options.selectedText + '</em>');
+            var $hide = self.$('.hide:first');
+            self.$srText = $hide.length ? $hide : $('<em class="hide">' + self.options.selectedText + '</em>');
 
-            if (self.options.allowHScroll) {
-                var $child = self.$el.children().first();
+            var $child = self.$el.children().eq(0);
+            if (!$child.is('ul')) {
                 self.options.tabsSelector = '>' + $child[0].tagName.toLowerCase() + self.options.tabsSelector;
                 if ($child.css('overflow') === 'hidden') {
                     $child.vcSmoothScroll();
@@ -6743,24 +6744,22 @@ define('ui/tab', ['jquery', 'vcui', 'ui/smoothScroll'], function ($, core) {
 
         update: function update() {
             var self = this;
-
             self._findControls();
             self._buildARIA();
         },
-
+        getSelectIdx:function getSelectIdx(){
+            return this.selectedIndex;
+        },
         _findControls: function _findControls() {
             var self = this;
             var selectors = [];
-
             self.$tabs = self.$(self.options.tabsSelector);
             self.$contents = $();
-
             // 탭버튼의 href에 있는 #아이디 를 가져와서 컨텐츠를 조회
             self.$tabs.each(function () {
                 var $tab = $(this),
                     $panel,
                     href = $tab.find('a').attr('href');
-
                 if (href && /^(#|\.)\w+/.test(href)) {
                     if (($panel = $tab.find('>div')).length) {
                         self.$contents = self.$contents.add($panel);
@@ -6769,30 +6768,24 @@ define('ui/tab', ['jquery', 'vcui', 'ui/smoothScroll'], function ($, core) {
                     }
                 }
             });
-
             if (!self.$contents.length) {
                 self.$contents = self.$('>' + prefixClass + 'panel');
             }
         },
-
         /**
          * @private
          */
         _bindEvents: function _bindEvents() {
             var self = this;
-
             self.on('click keydown', self.options.tabsSelector + '>a, ' + self.options.tabsSelector + '>button', function (e) {
-
                 switch (e.type) {
                     case 'click':
                         e.preventDefault();
-
                         self.select($(e.currentTarget).parent().index());
                         break;
                     case 'keydown':
                         var index = $(e.currentTarget).parent().index(),
                             newIndex;
-
                         switch (e.which) {
                             case core.keyCode.RIGHT:
                                 e.preventDefault();
@@ -6811,7 +6804,6 @@ define('ui/tab', ['jquery', 'vcui', 'ui/smoothScroll'], function ($, core) {
                 }
             });
         },
-
         /**
          * aria 속성 빌드
          * @private
@@ -6820,30 +6812,43 @@ define('ui/tab', ['jquery', 'vcui', 'ui/smoothScroll'], function ($, core) {
             var self = this,
                 tablistid = self.cid,
                 tabid;
-
             self.$el.attr('role', 'tablist');
             self.$tabs.each(function (i) {
-                tabid = tablistid + '_' + i;
-
-                $(this).attr({
-                    'id': tabid,
-                    'role': 'tab',
-                    'aria-selected': 'false',
-                    'aria-controls': tabid
-                });
-
+                tabid = $(this).children().attr('href').substr(1) || (tablistid + '_' + i);
+                $(this)
+                    .attr({
+                        'role': 'presentation'
+                    })
+                    .children()
+                    .attr({
+                        //'id': tabid,
+                        'role': 'tab',
+                        'aria-selected': 'false',
+                        'aria-controls': tabid
+                    });
                 if (!self.$contents.eq(i).attr('id')) {
                     self.$contents.eq(i).attr('id', tabid);
                 }
-
                 self.$contents.eq(i).attr({
                     'aria-labelledby': tabid,
                     'role': 'tabpanel',
-                    'aria-hidden': 'true'
+                    'aria-hidden': self.selectedIndex === i ? 'false' : 'true'
                 });
             });
         },
-
+        _updateTabHeight: function () {
+            var self = this;
+            var maxHeight = 0;
+            if (self.options.tabForceHeight) {
+                self.$tabs.find('a').css('height', '').each(function (i) {
+                    var h = $(this).height();
+                    if (h > maxHeight) {
+                        maxHeight = h;
+                    }
+                });
+                self.$tabs.find('a').css('height', maxHeight);
+            }
+        },
         /**
          * index에 해당하는 탭을 활성화
          * @param {number} index 탭버튼 인덱스
@@ -6854,57 +6859,84 @@ define('ui/tab', ['jquery', 'vcui', 'ui/smoothScroll'], function ($, core) {
          * // or
          * $('#tab').tab('instance').select(1);
          */
-        select: function select(index) {
+        select: function select(index, noTrigger) {
             var self = this,
                 e;
-            if (index < 0 || self.$tabs.length && index >= self.$tabs.length) {
-                throw new Error('index 가 범위를 벗어났습니다.');
+
+            if(!noTrigger){
+                //if (index < 0 || self.$tabs.length && index >= self.$tabs.length) {
+                if (self.$tabs.length && index >= self.$tabs.length) {
+                    return;
+                    //throw new Error('index 가 범위를 벗어났습니다.');
+                }
             }
+            
+            if(!noTrigger){
 
-            /**
-             * 탭이 바뀌기 직전에 발생. e.preventDefault()를 호출함으로써 탭변환을 취소할 수 있다.
-             * @event vcui.ui.Tab#tabbeforechange
-             * @type {object}
-             * @property {number} selectedIndex 선택된 탭버튼의 인덱스
-             */
-            self.triggerHandler(e = $.Event(eventBeforeChange), {
-                selectedIndex: index,
-                relatedTarget: self.$tabs.get(index),
-                button: self.$tabs.eq(self.selectedIndex).find('>a'),
-                content: self.$contents.eq(self.selectedIndex)
+                /**
+                 * 탭이 바뀌기 직전에 발생. e.preventDefault()를 호출함으로써 탭변환을 취소할 수 있다.
+                 * @event vcui.ui.Tab#tabbeforechange
+                 * @type {object}
+                 * @property {number} selectedIndex 선택된 탭버튼의 인덱스
+                 */
 
-            });
-            if (e.isDefaultPrevented()) {
-                return;
+                self.triggerHandler(e = $.Event(eventBeforeChange), {
+                    selectedIndex: index,
+                    relatedTarget: self.$tabs.get(index),
+                    button: self.$tabs.eq(index).find('>a'),
+                    content: self.$contents.eq(index)
+                });
+                if (e.isDefaultPrevented()) {
+                    return;
+                }
             }
 
             self.selectedIndex = index;
 
             var $a, $hide;
-            $a = self.$tabs.removeClass(selectedClass).attr('aria-selected', false).eq(index).addClass(selectedClass).attr('aria-selected', true).find('>a');
 
-            if (($hide = $a.find('.blind')).length) {
-                self.$tabs.not(self.$tabs.eq(index)).find('>a .blind').text("");
+            if(index<0){
+                $a = self.$tabs.removeClass(selectedClass).children('a, button').attr('aria-selected', false);
+            }else{
+                $a = self.$tabs.removeClass(selectedClass).children('a, button').attr('aria-selected', false).end().eq(index).addClass(selectedClass).children('a, button').attr('aria-selected', true);
+            }
+        
+
+            if (($hide = $a.find('.hide')).length) {
+                self.$tabs.not(self.$tabs.eq(index)).find('>a .hide').text("");
                 $hide.text(self.options.selectedText);
             } else {
                 $a.append(self.$srText);
             }
 
             // 컨텐츠가 li바깥에 위치한 탭인 경우
-            self.$contents.hide().eq(index).show();
+            if(index<0){
+                self.$contents.hide().attr('aria-hidden', 'true');
+            }else{
+                self.$contents.hide().attr('aria-hidden', 'true').eq(index).attr('aria-hidden', 'false').show();
+            }
 
-            /**
-             * 탭이 바뀌기 직전에 발생. e.preventDefault()를 호출함으로써 탭변환을 취소할 수 있다.
-             * @event vcui.ui.Tab#tabchange
-             * @type {object}
-             * @property {number} selectedIndex 선택된 탭버튼의 인덱스
-             */
-            self.triggerHandler(eventChanged, {
-                selectedIndex: index,
-                button: self.$tabs.eq(index).find('>a'),
-                content: self.$contents.eq(index)
-            });
+            self._updateTabHeight();
+
+            if(!noTrigger){
+                /**
+                 * 탭이 바뀌기 직전에 발생. e.preventDefault()를 호출함으로써 탭변환을 취소할 수 있다.
+                 * @event vcui.ui.Tab#tabchange
+                 * @type {object}
+                 * @property {number} selectedIndex 선택된 탭버튼의 인덱스
+                 */
+
+                //evt = $.Event(eventChanged)
+
+                self.triggerHandler(eventChanged, {
+                    selectedIndex: index,
+                    relatedTarget: self.$tabs.get(index),
+                    button: self.$tabs.eq(index).find('>a'),
+                    content: self.$contents.eq(index)
+                });
+            }
         }
+
     });
     ///////////////////////////////////////////////////////////////////////////////////////
 
