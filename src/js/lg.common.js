aaa791f917de2f6ef87af972150266aec58d8c87
정승우/(협력사) 책임/데이터통합Task(sw.jung@lge.com) <sw.jung@lge.com>
2020-08-28 17:45:41 +0900

    test

--- src/js/lg.common.js
+++ src/js/lg.common.js
@@ -1,53 +1,54 @@
-
 var lgkorUI = {
     template: $('<div class="template"></div>'),
     templateList: null,
 
     init: function(){
         vcui.require([
-                            'common/header', 
-                            'common/footer', 
-                            'ui/tab', 
-                            'ui/accordion', 
-                            'ui/carousel', 
-                            'ui/dropdown', 
-                            'ui/selectbox', 
-                            'ui/calendar', 
-                            'ui/lazyLoader'
-                            /*,
-                            "ui/videoBox",
-                            "ui/youtubeBox",
-                            "ui/modal"*/
-                        ], function (a) {
-            $('body').vcLazyLoader();
-            $('header').vcHeader(); //헤더 모듈 적용...
-            $('footer').vcFooter(); //푸터모듈 적용...
-    
-            //공통 UI 모듈 적용...
-            $('body').find('.ui_tab').vcTab();
-            $('body').find('.ui_accordion').vcAccordion();
-            $('body').find('.ui_carousel').vcCarousel();
-            $('body').find('.ui_dropdown').vcDropdown();
-            $('body').find('.ui_selectbox').vcSelectbox();
-            $('body').find('.ui_calendar').vcCalendar();
-            //$('body').find('.animation-box').vcVideoBox();
-            //$('body').find('.youtube-box').vcYoutubeBox();
-            console.log("succeass!!!!!!!!", a);
-        });
-
-        vcui.require([
+            'common/header', 
+            'common/footer', 
+            'ui/tab', 
+            'ui/accordion', 
+            'ui/carousel', 
+            'ui/dropdown', 
+            'ui/selectbox', 
+            'ui/calendar',
+            'ui/textControl',
+            'ui/scrollview', 
+            'ui/lazyLoader',
             "ui/videoBox",
             "ui/youtubeBox",
-            'ui/scrollview', 
-            'ui/textControl', 
-            "ui/modal"
-        ], function (a) {
+            "ui/modal",
+        ], function (header, footer, tab, accordion, carousel, dropdown, selectbox, calendar, textControl, scollview, lazyLoader, videoBox, youtubeBox, modal) {
+            console.log("loaded: ",lazyLoader);
+            $('body').vcLazyLoader();
+            console.log("loaded: ", header);
+            $('header').vcHeader(); //헤더 모듈 적용...
+            console.log("loaded: ", footer);
+            $('footer').vcFooter(); //푸터모듈 적용...
+
+            //공통 UI 모듈 적용...
+            console.log("loaded: ", tab);
+            $('body').find('.ui_tab').vcTab();
+            console.log("loaded: ", accordion);
+            $('body').find('.ui_accordion').vcAccordion();
+            console.log("loaded: ", carousel);
+            $('body').find('.ui_carousel').vcCarousel();
+            console.log("loaded: ", dropdown);
+            $('body').find('.ui_dropdown').vcDropdown();
+            console.log("loaded: ", selectbox);
+            $('body').find('.ui_selectbox').vcSelectbox();
+            console.log("loaded: ", calendar);
+            $('body').find('.ui_calendar').vcCalendar();
+            console.log("loaded: ", textControl);
             $('body').find('.ui_textcontrol').vcTextControl();
+            console.log("loaded: ", scollview);
             $('body').find('.ui_scrollview').vcScrollview();
-$('body').find('.animation-box').vcVideoBox();
-$('body').find('.youtube-box').vcYoutubeBox();
-console.log("succeass???????????", a);
-});
+            console.log("loaded: ", videoBox);
+            $('body').find('.animation-box').vcVideoBox();
+            console.log("loaded: ", youtubeBox);
+            $('body').find('.youtube-box').vcYoutubeBox();
+            console.log("loaded: ", modal);
+        });
     },
 
     //template html 리스트 파일 로드...
