a2715cc565c45ad1c0799982c7a2dbf0c145f531
pparks <pparks@pparksui-MacBookPro.local>
2020-08-28 17:28:48 +0900

    debug1

--- src/js/lg.common.js
+++ src/js/lg.common.js
@@ -13,14 +13,12 @@ var lgkorUI = {
                             'ui/dropdown', 
                             'ui/selectbox', 
                             'ui/calendar', 
-                            'ui/textControl', 
-                            'ui/scrollview', 
                             'ui/lazyLoader'
                             /*,
                             "ui/videoBox",
                             "ui/youtubeBox",
                             "ui/modal"*/
-                        ], function () {
+                        ], function (a) {
             $('body').vcLazyLoader();
             $('header').vcHeader(); //헤더 모듈 적용...
             $('footer').vcFooter(); //푸터모듈 적용...
@@ -32,21 +30,23 @@ var lgkorUI = {
             $('body').find('.ui_dropdown').vcDropdown();
             $('body').find('.ui_selectbox').vcSelectbox();
             $('body').find('.ui_calendar').vcCalendar();
-            $('body').find('.ui_textcontrol').vcTextControl();
-            $('body').find('.ui_scrollview').vcScrollview();
             //$('body').find('.animation-box').vcVideoBox();
             //$('body').find('.youtube-box').vcYoutubeBox();
-            console.log("succeass!!!!!!!!");
+            console.log("succeass!!!!!!!!", a);
         });
 
         vcui.require([
             "ui/videoBox",
             "ui/youtubeBox",
+            'ui/scrollview', 
+            'ui/textControl', 
             "ui/modal"
-        ], function () {
+        ], function (a) {
+            $('body').find('.ui_textcontrol').vcTextControl();
+            $('body').find('.ui_scrollview').vcScrollview();
 $('body').find('.animation-box').vcVideoBox();
 $('body').find('.youtube-box').vcYoutubeBox();
-console.log("succeass???????????");
+console.log("succeass???????????", a);
 });
     },
 
