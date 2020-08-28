112070c3882359658904980a1743ffe994a08464
pparks <pparks@pparksui-MacBookPro.local>
2020-08-28 17:37:39 +0900

    debug2

--- src/js/lg.common.js
+++ src/js/lg.common.js
@@ -2,23 +2,24 @@
 var lgkorUI = {
     template: $('<div class="template"></div>'),
     templateList: null,
-
     init: function(){
         vcui.require([
-                            'common/header', 
-                            'common/footer', 
-                            'ui/tab', 
-                            'ui/accordion', 
-                            'ui/carousel', 
-                            'ui/dropdown', 
+                            //'common/header', 
+                            //'common/footer', 
+                            //'ui/tab', 
+                            //'ui/accordion', 
+                            //'ui/carousel', 
+                            //'ui/dropdown', 
                             'ui/selectbox', 
                             'ui/calendar', 
-                            'ui/lazyLoader'
-                            /*,
+                            'ui/textControl', 
+                            'ui/scrollview', 
+                            //'ui/lazyLoader',
                             "ui/videoBox",
                             "ui/youtubeBox",
-                            "ui/modal"*/
-                        ], function (a) {
+                            //"ui/modal"
+                        ], function () {
+                            /*
             $('body').vcLazyLoader();
             $('header').vcHeader(); //헤더 모듈 적용...
             $('footer').vcFooter(); //푸터모듈 적용...
@@ -30,24 +31,12 @@ var lgkorUI = {
             $('body').find('.ui_dropdown').vcDropdown();
             $('body').find('.ui_selectbox').vcSelectbox();
             $('body').find('.ui_calendar').vcCalendar();
-            //$('body').find('.animation-box').vcVideoBox();
-            //$('body').find('.youtube-box').vcYoutubeBox();
-            console.log("succeass!!!!!!!!", a);
-        });
-
-        vcui.require([
-            "ui/videoBox",
-            "ui/youtubeBox",
-            'ui/scrollview', 
-            'ui/textControl', 
-            "ui/modal"
-        ], function (a) {
             $('body').find('.ui_textcontrol').vcTextControl();
             $('body').find('.ui_scrollview').vcScrollview();
-$('body').find('.animation-box').vcVideoBox();
-$('body').find('.youtube-box').vcYoutubeBox();
-console.log("succeass???????????", a);
-});
+            $('body').find('.animation-box').vcVideoBox();
+            $('body').find('.youtube-box').vcYoutubeBox();
+            */
+        });
     },
 
     //template html 리스트 파일 로드...
