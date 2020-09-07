86ae9e0048b80ea726aa219cb5a13f4f22396626
Yun <>
2020-09-07 17:31:37 +0900

    data js

--- src/guide/data.js
+++ src/guide/data.js
@@ -31,14 +31,14 @@ var res = {
 			"KRC0018_Image - Layered Text" : ["/html/components/KRC0018.html","","검수완료"],
 			"KRC0019_Slider Image - Top Bottom Text" : ["/html/components/KRC0019.html","","검수완료"],
 			"KRC0020_Slider Image - Left RIght Text" : ["/html/components/KRC0020.html","","검수완료"],
-			"KRC0021_Carousel Block Image - Text" : ["/html/components/KRC0021.html","","진행중"],
-			"KRC0022_Large Image - Vertical Pickers" : ["/html/components/KRC0022.html","","진행중"],
-			"KRC0023_Large Image - Horizontal Pickers" : ["/html/components/KRC0023.html","","진행중"],
-			"KRC0024_Hero Image" : ["/html/components/KRC0024.html","","진행중"],
+			"KRC0021_Carousel Block Image - Text" : ["/html/components/KRC0021.html","","검수완료"],
+			"KRC0022_Large Image - Vertical Pickers" : ["/html/components/KRC0022.html","","검수완료"],
+			"KRC0023_Large Image - Horizontal Pickers" : ["/html/components/KRC0023.html","","검수완료"],
+			"KRC0024_Hero Image" : ["/html/components/KRC0024.html","","검수완료"],
 			"KRC0025_Scroll Image" : ["/html/components/KRC0025.html","","진행중"],
-			"KRC0026_Interactive Gallery Image" : ["/html/components/KRC0026.html","","진행중"],
+			"KRC0026_Interactive Gallery Image" : ["/html/components/KRC0026.html","","검수완료"],
 			"KRC0029_Block Image(2 - Text Overlay)" : ["/html/components/KRC0029.html","","진행중"],
-			"KRC0030_Feature Icon Tab" : ["/html/components/KRC0030.html","","진행중"],
+			"KRC0030_Feature Icon Tab" : ["/html/components/KRC0030.html","","검수완료"],
 			"KRC0031_Block Image (2 to 3 - CTA)" : ["/html/components/KRC0031.html","","진행중"],
 			"KRC0032_Section Title" : ["/html/components/KRC0032.html","","진행중"],
 			"KRC0037_Carousel BG Image " : ["/html/components/KRC0037.html","","진행중"],
@@ -51,10 +51,10 @@ var res = {
 		"Page" : {
 			"이벤트/혜택" : {
 				"멤버십" : {
-					"멤버십 메인" : ["/html/membership/mem01.html","","진행중"],
-					"멤버십 혜택" : ["/html/membership/mem02_1.html","","진행중"],
-					"VIP 멤버십 혜택" : ["/html/membership/mem02_2.html","","진행중"],
-					"멤버십 가입안내" : ["/html/membership/mem03.html","","진행중"]
+					"멤버십 메인" : ["/html/membership/mem01.html","","검수요청"],
+					"멤버십 혜택" : ["/html/membership/mem02_1.html","","검수요청"],
+					"VIP 멤버십 혜택" : ["/html/membership/mem02_2.html","","검수요청"],
+					"멤버십 가입안내" : ["/html/membership/mem03.html","","검수요청"]
 				}
 			}
 			// "페이지1" : ["./html/gr/M.GR-5L.html","","개발전달후수정"],
