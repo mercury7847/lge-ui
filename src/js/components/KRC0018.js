043f0b7d658098b81fbeb7112c43db9d57ac30a5
pparks <pparks@pparksui-MacBookPro.local>
2020-09-08 16:26:27 +0900

    js 추가

---
+++ src/js/components/KRC0018.js
@@ -0,0 +1,5 @@
+$(window).ready(function(){
+    if(!document.querySelector('.KRC0018')) return false;
+
+    $('.KRC0018').buildCommonUI();
+})
---
+++ src/js/components/KRC0029.js
@@ -0,0 +1,5 @@
+$(window).ready(function(){
+    if(!document.querySelector('.KRC0029')) return false;
+
+    $('.KRC0029').buildCommonUI();
+})
