6d586a6812c6b96fd7c652cf342e3d74cbf6ceae
pparks <pparks@pparksui-MacBookPro.local>
2020-09-11 15:14:41 +0900

    footer

--- src/js/common/footer.js
+++ src/js/common/footer.js
@@ -13,9 +13,22 @@ vcui.define('common/footer', ['jquery', 'vcui', 'ui/dropdown' ], function ($, co
             if (self.supr(el, options) === false) {
                 return;
             };
+            
+            self._resize();
+            $(window).trigger('addResizeCallback', self._resize.bind(self));
         },
 
         _resize: function(){
+            var self = this,
+                winwidth;
+
+            winwidth = $(window).outerWidth(true);
+            if(winwidth > 767){
+                self.$el.find('.ui_footer_accordion').vcAccordion('destory');
+            } else{
+                console.log("GGG")
+                self.$el.find('.ui_footer_accordion').vcAccordion();
+            }
         }
     });
 
