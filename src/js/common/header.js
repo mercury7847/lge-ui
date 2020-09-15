881a787c0b23408cfef92f7e8797fd79bbce4b81
pparks <pparks@pparksui-MacBookPro.local>
2020-09-15 21:54:19 +0900

    gnb

--- src/js/common/header.js
+++ src/js/common/header.js
@@ -14,21 +14,11 @@ vcui.define('common/header', ['jquery', 'vcui'], function ($, core) {
                 return;
             };
 
-            self.isTrans = false;
-
-            self.isHover = false;
-            self.outTimer = null;
-
-            self.isCategoryHover = false;
-            self.categoryOutTimer = null;
-
-            self.gnbMode = "pc";
-
-            self.prodWidth = 0;
-            
-            self._setting();
-            self._bindEvents();
-            self._resize();
+            vcui.require(['ui/carousel', 'ui/smoothScroll'], function () {            
+                self._setting();
+                self._bindEvents();
+                self._resize();
+            });
         },
 
         _setting: function(){
@@ -39,32 +29,16 @@ vcui.define('common/header', ['jquery', 'vcui'], function ($, core) {
             self.$naviWrapper = self.$el.find(".header-bottom");
             self.$navItems = self.$el.find('.nav-wrap .nav > li');
             
-
-
-
-            self.$prodWrapper = self.$naviWrapper.find('.nav-category-product');
-            self.$product = self.$prodWrapper.find('.nav-item');
-            self.$categoryWrapper = self.$prodWrapper.find('.nav-category-container');
-            self.$category = self.$categoryWrapper.find('> ul');
-            self.$categoryItems = self.$category.children();
-
             self.$hamburger = self.$el.find('.mobile-nav-button');
-
-            self.prodWidth = self.$categoryWrapper.width();
         },
 
         _resize: function(){
             var self = this;
             var winwidth = $(window).outerWidth(true);
             if(winwidth > 767){
-                self.gnbMode = "pc";
-
                 self._hamburgerDisabled();
                 self._addGnbEvents();
             } else{
-                self.gnbMode = "m"; 
-
-                self._gnbInit();
                 self._removeGnbEvents();
             }
         },
@@ -87,37 +61,29 @@ vcui.define('common/header', ['jquery', 'vcui'], function ($, core) {
             $(window).on('resize', function(){
                 self._resize();
             });
+
+            $('.mobile-category-container .category').vcSmoothScroll();
+
+            $('.mobile-nav-category.is-depth > a').on('click', function(e){
+                e.preventDefault();
+
+                $(this).parent().find('.nav-category-container').toggle();
+            });
         },
 
         _addGnbEvents: function(){
             var self = this;
 
-            self.$navItems.each(function(idx, item){
-                var itemHeight = $(item).outerHeight(true);
-                var wid = 0;
-                
-                /******************************/
+            self.$navItems.each(function(idx, item){              
                 $(item).find('> .nav-category-container').css('display', 'inline-block');
-                $(item).find('> .nav-category-container > ul > li').each(function(cdx, child){
-                    var pos = wid;
-                    var childwidth = $(child).outerWidth(true);
-                    wid += childwidth;
-                    $(child).css({
-                        //position: 'relative',
-                        //left: pos
-                    });                    
-                });
+                var categorywidth = $(item).find('> .nav-category-container').outerWidth(true);
                 $(item).find('> .nav-category-container').css({
-                    position: 'relative',
                     overflow: 'hidden',
                     'vertical-align': 'top',
                     width: 0,
                     display: 'none'
-                    
-                    //height: itemHeight
                 });
                 $(item).find('> .nav-category-container > ul').css({
-                    position: 'relative',
                     'vertical-align': 'top',
                     width: '100%'
                 });
@@ -125,11 +91,19 @@ vcui.define('common/header', ['jquery', 'vcui'], function ($, core) {
 
                 $(item).css({'vertical-align':  'top'});
 
-                $(item).data('subwidth', wid);
+                $(item).data('subwidth', categorywidth);
                 $(item).on('mouseover', function(e){
                     self._setOver(this);
                 }).on('mouseout', function(e){    
-                    self._setOutTimer(this);
+                    self._setOut(this);
+                });
+
+                $(item).find('> .nav-category-container > ul >li').each(function(cdx, child){
+                    $(child).on('mouseover', function(e){
+                        self._setOver(this);
+                    }).on('mouseout', function(){
+                        self._setOut(this);
+                    })
                 });
             });
         },
@@ -137,7 +111,17 @@ vcui.define('common/header', ['jquery', 'vcui'], function ($, core) {
         _removeGnbEvents: function(){
             var self = this;
 
-            self.$navItems.off('mouseover mouseout');
+            self.$navItems.each(function(idx, item){              
+                $(item).find('> .nav-category-container').removeAttr('style');
+                $(item).find('> .nav-category-container > ul').removeAttr('style');
+                $(item).find('> a').removeAttr('style');
+                $(item).removeAttr('style');
+
+                $(item).off('mouseover mouseout');
+                $(item).find('> .nav-category-container > ul >li').each(function(cdx, child){
+                    $(child).off('mouseover mouseout');
+                });
+            });
         },
 
         _mypageToggle: function(target){
@@ -189,109 +173,40 @@ vcui.define('common/header', ['jquery', 'vcui'], function ($, core) {
             if($('html').hasClass('scroll-fixed')) $('html').removeClass('scroll-fixed');
         },
 
-        _gnbInit: function(){
-            var self = this;
-
-            self._removeOutTimer();
-            self._setOut();
-            
-            self._setRemoveCategoryOutTimer();
-            self._setCategoryOut();
-        },
-
-        _setCategoryOver: function(idx){
-            var self = this;
-
-            if(self.gnbMode == "pc"){
-                self.isCategoryHover = true;
-    
-                var categoryItem = self.$categoryItems.eq(idx);
-                var categoryAtag = categoryItem.find('.super-category-item');
-                if(!categoryAtag.hasClass('active')) categoryAtag.addClass('active');
-            }
-        },
-
-        _setCategoryOutTimer: function(idx){
-            var self = this;
-
-            if(self.gnbMode == "pc"){
-                self.categoryOutTimer = setTimeout(function(){
-                    self._setCategoryOut(idx);
-                }, 128);
-            }
-        },
-
-        _setRemoveCategoryOutTimer: function(){
-            var self = this;
-
-            if(self.categoryOutTimer != null){
-                clearTimeout(self.categoryOutTimer);
-
-                self.categoryOutTimer = null;
-            }
-        },
-
-        _setCategoryOut: function(idx){
-            var self = this;
-
-            self.isCategoryHover = false;
-
-            if(self.gnbMode == "pc"){
-                var categoryItem = self.$categoryItems.eq(idx);
-                var categoryAtag = categoryItem.find('.super-category-item');
-                if(categoryAtag.hasClass('active')) categoryAtag.removeClass('active');
-            }
-        },
-
         _setOver: function(item){
             var self = this;
 
-            var subwidth = $(item).data('subwidth');
+            $(item).find('> a').addClass('active'); 
 
-            $(item).find('> a').addClass('active');
-    
-            $(item).find('> .nav-category-container').stop().css('display', 'inline-block').animate({width:subwidth}, 200);
-        },
+            var catecontainer = $(item).find('> .nav-category-container');
+            if(catecontainer.length){
+                var subwidth = $(item).data('subwidth');           
+                $(item).find('> .nav-category-container').stop().css('display', 'inline-block').animate({width:subwidth}, 200);
+            }
 
-        _setOutTimer: function(item){
-            var self = this;
-
-            $(item).find('> .nav-category-container').stop().animate({width:0}, 150, function(){
-                $(item).find('> a').removeClass('active');
-                $(item).find('> .nav-category-container').css('display', 'none');
-            });
-        },
-
-        _removeOutTimer: function(){
-            var self = this;
-
-            if(self.gnbMode == "pc" && self.outTimer != null){
-                clearTimeout(self.outTimer);
-
-                self.outTimer = null;
+            var categoryLayer = $(item).find('> .nav-category-layer');
+            if(categoryLayer.length){
+                categoryLayer.find('.ui_carousel_slider').vcCarousel({
+                    infinite: false,
+                    swipeToSlide: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                });
+                categoryLayer.find('.ui_carousel_list').css('overflow', 'hidden');
             }
         },
 
-        _setOut: function(){
-            var self = this;
-            
-            self.isHover = false;
-
-            if(self.gnbMode == "pc"){
-                self._setProductActiveToggle(false);
-            }
-        },
-
-        _setProductActiveToggle: function(active){
+        _setOut: function(item){
             var self = this;
 
-            var replaceText = self.$product.find('.blind');
-            if(active){
-                if(!self.$product.hasClass('active')) self.$product.addClass('active');
-                replaceText.text('제품 메뉴 닫기');
+            var catecontainer = $(item).find('> .nav-category-container');
+            if(catecontainer.length){
+                catecontainer.stop().animate({width:0}, 150, function(){
+                    $(item).find('> a').removeClass('active');
+                    $(item).find('> .nav-category-container').css('display', 'none');
+                });
             } else{
-                if(self.$product.hasClass('active')) self.$product.removeClass('active');
-                replaceText.text('제품 메뉴 열기');
+                $(item).find('> a').removeClass('active');
             }
         }
     });
