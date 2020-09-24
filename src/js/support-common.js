4e7b02f542fb9e9c375f9e9835caaaeeb303f86a
전호근/(협력사) 과장/디지털플랫폼팀(hogeun.jeon@lge.com) <hogeun.jeon@lge.com>
2020-09-24 15:05:54 +0900

    CS 제품 문제해결

--- src/js/support-common.js
+++ src/js/support-common.js
@@ -86,68 +86,81 @@ CS.MD.setPagination = function() {
     var pluginName = 'pagination';
 
     function Plugin(elem, opt, callback) {
-        var _this = this,
+        var self = this,
             element = this.element = elem,
             $element = $(element),
             $pageList, $prev, $next,
-            currentPage, firstPage, lastPage, totalPage;
+            currentPage, startPage, endPage, totalPage;
 
         var defaults = {
+            page: 1,
+            startPage: 1,
+            endPage: 5,
+            totalCount: 59,
             pageToShow: 5,
-            interval: 4,
+            nextBtn: true,
+            prevBtn: false,
             prevClass: '.prev',
             nextClass: '.next',
             disabledClass: 'disabled'
         };
 
-        defaults = $.extend({}, defaults, opt);
+        self.options = $.extend({}, defaults, opt);
 
-        function initialize() {
-            $pageList = $element.find('.page_num'),
-            $prev = $element.find(defaults.prevClass),
-            $next = $element.find(defaults.nextClass),
-
-            totalPage = parseInt($pageList.find('>:last-child').text().replace(/[^0-9]/g,''));
-            firstPage = currentPage = 1;
-            lastPage = totalPage <= defaults.pageToShow ? totalPage : firstPage + defaults.interval;
+        function _initialize() {
+            $pageList = $element.find('.page_num');
+            $prev = $element.find(self.options.prevClass);
+            $next = $element.find(self.options.nextClass);
         }
+        self._update = function() {
+            var startPage = self.options.startPage,
+                endPage = self.options.endPage,
+                page = self.options.page,
+                html = '';
 
-        function drawPagination(e) {
+            for (var i = startPage; i <= endPage; i++) {
+                if (page === i) {
+                    html += '<strong><span class="blind">현재 페이지</span>' + i + '</strong>';
+                } else {
+                    html += '<a href="#" title="' + i + '페이지 보기">' + i + '</a>';
+                }
+            }
+            
+            self.options.prevBtn ? $prev.attr('aria-disabled', false) : $prev.attr('aria-disabled', true) 
+            self.options.nextBtn ? $next.attr('aria-disabled', false) : $next.attr('aria-disabled', true)
+            $pageList.html(html);
+        }
+        self._move = function() {
             var $target, html = '';
 
-            if (e) {
-                e.preventDefault();
-                $target = $(e.currentTarget);
-            }
-
             if ($target) {
-                if ($target.is(defaults.prevClass)) {
-                    if (firstPage === 1) return false;
+                if ($target.is(self.options.prevClass)) {
+                    if (startPage === 1) return false;
 
-                    firstPage -= defaults.pageToShow;
-                    lastPage = firstPage + defaults.interval;
-                } else if ($target.is(defaults.nextClass)) { 
-                    if (lastPage === totalPage) return false;
+                    startPage -= self.options.pageToShow;
+                    endPage = startPage + self.options.pageToShow - 1;
+                } else if ($target.is(self.options.nextClass)) { 
+                    if (endPage === totalPage) return false;
 
-                    firstPage += defaults.pageToShow;
-                    lastPage = firstPage + defaults.interval;
+                    startPage += self.options.pageToShow;
+                    endPage = startPage + self.options.pageToShow - 1;
 
-                    lastPage >= totalPage && (lastPage = totalPage);
+                    endPage >= totalPage && (endPage = totalPage);
                 } else {
                     currentPage = parseInt($target.text());
 
                     if (currentPage === totalPage) {
-                        firstPage = currentPage - (currentPage - 1) % defaults.pageToShow;
-					    lastPage = currentPage;
+                        startPage = currentPage - (currentPage - 1) % self.options.pageToShow;
+					    endPage = currentPage;
                     }
                     callback && callback();
                 }
             }
 
-            firstPage === 1 ? $prev.attr('aria-disabled', true) : $prev.attr('aria-disabled', false);
-            lastPage >= totalPage ? $next.attr('aria-disabled', true) : $next.attr('aria-disabled', false);
+            startPage === 1 ? $prev.attr('aria-disabled', true) : $prev.attr('aria-disabled', false);
+            endPage >= totalPage ? $next.attr('aria-disabled', true) : $next.attr('aria-disabled', false);
 
-            for (var i = firstPage; i <= lastPage; i++) {
+            for (var i = startPage; i <= endPage; i++) {
                 if (currentPage === i) {
                     html += '<strong><span class="blind">현재 페이지</span>' + i + '</strong>';
                 } else {
@@ -155,7 +168,7 @@ CS.MD.setPagination = function() {
                 }
             }
 
-            if (defaults.pageToShow < totalPage && lastPage < totalPage) {
+            if (self.options.pageToShow < totalPage && endPage < totalPage) {
                 html += '<span class="dot">...</span>';
 
                 if (currentPage === totalPage) {
@@ -168,28 +181,28 @@ CS.MD.setPagination = function() {
             $pageList.html(html);
         }
 
-        function setEventListener() {
-            $prev.on('click.page', drawPagination);
-            $next.on('click.page', drawPagination);
-            $pageList.on('click.page', 'a', drawPagination);
-        }
-
-        initialize();
-        setEventListener();
+        _initialize();
     }
 
     Plugin.prototype = {
-        reset: function() {
-            firstPage = currentPage = 1;
-            lastPage = totalPage <= defaults.pageToShow ? totalPage : firstPage + defaults.interval;
-            drawPagination();
+        update: function(data) {
+            var self = this;
+
+            self.options = $.extend({}, self.defaults, data);
+            self._update();
         },
+        reset: function() {
+            startPage = currentPage = 1;
+            endPage = totalPage <= this.options.pageToShow ? totalPage : startPage + this.options.pageToShow - 1;
+            
+            this._move();
+        }
     }
 
     $.fn[pluginName] = function(options, callback) {
         return this.each(function() {
             if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin(this, options, callback));
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
             }
         });
     }
@@ -262,10 +275,34 @@ CS.MD.setPagination = function() {
         });
     }
 
+    function test() {
+        if (!$('.related-info-wrap').length) return false;
+
+        vcui.require(['ui/carousel'], function () {
+            $('.ui_carousel_slider').vcCarousel({
+                infinite: false,
+                swipeToSlide: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                responsive: [
+                    {
+                        breakpoint: 767,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            variableWidth: true,
+                        }
+                    }
+                ]
+            });
+        })
+    }
+
     function commonInit(){
         setTableScrollbar();
         checkPrivacy();
-        selectEngineer()
+        selectEngineer();
+        test();
     }
 
     // CS.UI.elem.$win.ready( commonInit );
