095d6832dbd1ee904d45d12ee07353ed3ace948c
정은선 <secret2004@vinylc.com>
2020-09-25 16:27:06 +0900

    매장찾기 작업중

--- src/js/ui/carousel.js
+++ src/js/ui/carousel.js
@@ -151,7 +151,7 @@ vcui.define('ui/carousel', ['jquery', 'vcui'], function ($, core) {
             slidesPerRow: 1,                // rows가 1보다 클 경우 행별 슬라이드 수
             slidesToShow: 1,                // 표시할 슬라이드 수
             slidesToScroll: 1,              // 슬라이딩될 때 한번에 움직일 갯수
-            speed: 800,                     // 슬라이딩 속도
+            speed: 500,                     // 슬라이딩 속도
             swipe: true,                    // 스와이핑 허용 여부
             swipeToSlide: false,            // 사용자가 slidesToScroll과 관계없이 슬라이드로 직접 드래그 또는 스 와이프 할 수 있도록 허용
             touchMove: true,                // 터치로 슬라이드 모션 사용
