32162b1faff1822e44fe31d03ac87b63938d630b
정승우/(협력사) 책임/데이터통합Task(sw.jung@lge.com) <sw.jung@lge.com>
2020-10-06 18:22:40 +0900

    cart

--- src/js/cart/cart.js
+++ src/js/cart/cart.js
@@ -1,23 +1,52 @@
 (function() {
-    console.log("????");
     var firstLoad = true;
     var cartUrl = "";
     var removeUrl = "";
 
-    var paymentTemplate =
-                '<li class="list"><div class="notice-box">'+
-                '<span class="icons">알림 확인 상태 : <em class="{{read}}">{{readDescription}}</em></span>'+
-                '<p class="date"><span class="blind">알림 도착 날짜 및 시간</span>{{alarmDate}}</p>'+
-                '<div class="box"><span class="blind">알림 내용</span>'+
-                '<p class="tit">{{title}}</p><p class="desc">{{description}}</p>'+
-                '<div class="more-view"><p class="name"><span class="blind">제품명</span>{{productName}}</p>'+
-                '<ul class="info"><li><dl><dt>제품코드</dt>dd>{{procuctCode}}<i aria-hidden="true">&bull;</i>{{categoryName}}</dd></dl></li>'+
-                '<li><dl><dt>구매수량</dt><dd>{{purchaseQuantity}}</dd></dl></li>'+
-                '<li><dl><dt>구매매장</dt><dd>{{purchaseStore}}</dd></dl></li>'+
-                '<li><dl><dt>구매일자</dt><dd>{{purchaseDate}}</dd></dl></li></ul></div>'+
-                '<div class="bottom-btn"><a href="{{linkUrl}}" class="btn-link">결제내역 자세히보기</a></div>'+
-                '<button type="button" class="btn-del" data-id="{{id}}"><span>알림 삭제</span></button>'+
-                '</div></div></li>';
+    var cartItemTemplate =
+                '<div class="cart-item">' +
+                '<div class="product-check"><span class="chk-wrap">' +
+                    '<input type="checkbox" id="selectitem{{index}}" name="selectitem">' +
+                    '<label for="selectitem{{index}}"><span class="blind">선택안함</span></label>' +
+                '</span></div>' +
+                '<div class="product-info">' +
+                    '<div class="item-image"><img src="{{imageUrl}}" alt="{{imageAlt}}"></div><div class="item-name">{{modelName}}</div>' +
+                    '<div class="item-options"><div class="sku">{{modelNumber}}</div><div class="sibling-option">{{modelOption}}</div>' +
+                '</div>' +
+                '<div class="quantity-wrap">' +
+                    '<div class="select-quantity"><div class="inner">' +
+                        '<button type="button" class="minus"><span class="blind">뺴기</span></button>' +
+                        '<input type="number" class="quantity" title="수량 입력" value="{{Quantity}}">' +
+                        '<button type="button" class="plus"><span class="blind">더하기</span></button>' +
+                    '</div></div>' +
+                '</div><div class="delivery-option">' +
+                    '{{#if (reservationAdvance)}}<a href="{{reservationAdvance}}" class="btn-text">사전방문 예약</a>{{/if}}' +
+                    '{{#if (deliveryDesired)}}<a href="{{deliveryDesired}}" class="btn-text">배송희망일 지정</a>{{/if}}' +
+                '</div></div>' +
+                '{{#if (coupon.length > 0)}}' +
+                '<div class="select-coupon"><div class="select-wrap">' +
+                    '<select class="ui_selectbox" id="selectCoupon" title="쿠폰 선택">' +
+                        '<option value="" class="placeholder">쿠폰을 선택해주세요.</option>' +
+                        '{{#each item in coupon}}<option value="{{item.couponId}}">{{item.couponName}}</option>{{/each}}' +
+                    '</select></div>' +
+                '</div>'+
+                '{{/if}}' +
+                '<div class="product-payment">' +
+                    '<div class="inner">' +
+                        '<div class="calc-amount">{{order}}원 {{#if (sale)}}<em>- {{sale}}</em> {{/if}}+ 배송비 {{shipping}}원</div>' +
+                        '<div class="amount">' +
+                            '{{#if (order != total)}}<span class="purchase-price"><em class="blind">판매가격</em>{{order}}원</span>{{/if}}' +
+                            '<span class="total-price"><em class="blind">총 금액</em>{{total}}원</span>' +
+                        '</div>' +
+                        '<div class="btn-purchase"><a href="#" class="btn bd-pink btn-small">바로구매</a></div>' +
+                    '</div>' +
+                '</div>' +
+                '<div class="product-wish"><span class="chk-wish-wrap">' +
+                    '<input type="checkbox" id="favoritem{{index}}" name="favoritem" {{#if (favorite)}}checked{{/if}}>' +
+                    '<label for="favoritem{{index}}"><span class="blind">찜하기</span></label>' +
+                '</span></div>' +
+                '<div class="item-delete"><button type="button" class="btn-delete"><span class="blind">상품 삭제</span></button></div>' +
+                '</div>';
 
     function noData(visible) {
         /*
@@ -34,8 +63,6 @@
     }
 
     function requestChangeCartData() {
-        console.log(cartUrl);
-
         $.ajax({
             url: cartUrl
         }).done(function (d) {
@@ -46,12 +73,42 @@
 
             var data = d.data;
             console.log(data);
-            var arr = data.cart instanceof Array ? data : [];
+            
+            //구매/렌탈 카운트
+            self.$buyCount.text('구매 ' + data.buyCount + (data.buyCount>0?"+":""));
+            self.$rentalCount.text('렌탈(케어솔루션) ' + data.rentalCount + (data.rentalCount>0?"+":""));
+            
+            if(data.isMember) {
+                self.$nonMemberInfo.hide();
+                if(data.address1) {
+                    self.$memberAddressInfo.find('dd.text').html(data.address1+'<span class="detail-address">'+data.address2+'</span>');
+                    self.$memberAddressInfo.show();
+                    self.$memberNoAddressInfo.hide();
+                } else {
+                    self.$memberAddressInfo.hide();
+                    self.$memberNoAddressInfo.show();
+                }
+            } else {
+                self.$nonMemberInfo.show();
+                self.$memberAddressInfo.hide();
+                self.$memberNoAddressInfo.hide();
+            }
+            
+            var arr = data.cart instanceof Array ? data.cart : [];
 
-            var contentHtml = "";
+            //var contentHtml = "";
 
             if(arr.length > 0) {
                 noData(false);
+                self.$cartList.empty();
+                arr.forEach(function(item, index) {
+                    item.index = "" + index;
+                    //item.totalOnly = (item.order == item.total);
+                    //console.log(item);
+                    var test = vcui.template(cartItemTemplate,item);
+                    self.$cartList.append(test);
+                    //console.log(test);
+                });
             } else {
                 noData(true);
             }
@@ -72,10 +129,17 @@
                 var cartContent = $('div.contents.cart');
                 cartUrl = cartContent.attr('data-url-cart');
                 removeUrl = cartContent.attr('data-url-remove');
+                self.$buyCount = $('div.page-header div.tabs-wrap ul.tabs li:nth-child(1) a');
+                self.$rentalCount = $('div.page-header div.tabs-wrap ul.tabs li:nth-child(2) a');
+                //
+                self.$nonMemberInfo = $('div.info-box div:nth-child(1)');
+                self.$memberAddressInfo = $('div.info-box div:nth-child(2)');
+                self.$memberNoAddressInfo = $('div.info-box div:nth-child(3)');
+                //
+                self.$cartList = $('div.cart-list');
 
                 this.bindEvents();
 
-                console.log('cart');
                 requestChangeCartData();
             },
 
