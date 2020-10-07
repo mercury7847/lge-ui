e4409753e884b7f57a9cfbd1994524f0f7b87062
Yun <>
2020-10-07 17:58:54 +0900

    KRP0014

--- src/guide/data.js
+++ src/guide/data.js
@@ -72,6 +72,7 @@ var res = {
 			"KRP0009_Product List Products List : No Filter" : ["/html/components/KRP0009.html","","진행중"],
 			"KRP0010_Product Summary (no-img)" : ["/html/components/KRP0010.html","","진행중"],
 			"KRP0013_PDP Bundle" : ["/html/components/KRP0013.html","","검수완료"],
+			"KRP0014_PDP Specs" : ["/html/components/KRP0014.html","","진행중"],
 			"KRP0016_PDP Support - B2C" : ["/html/components/KRP0016.html","","검수완료"],
 			"KRP0017_Promotion List" : ["/html/components/KRP0017.html","","검수완료"],
 			"KRP0018_Sticky Compare" : ["/html/components/KRP0018.html","",""],
