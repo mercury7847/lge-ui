1552aedf5dfa31e02be446f289b73be4eadb8791
정은선 <secret2004@vinylc.com>
2020-10-13 10:58:53 +0900

    data.js 수정

--- src/guide/data.js
+++ src/guide/data.js
@@ -80,12 +80,14 @@ var res = {
 			"KRP0019_Social Share_Dynamic" : ["/html/components/KRP0019.html","","진행중"],
 			"KRP0020_Sitemap_Dynamic" : ["/html/components/KRP0020.html","","검수완료"],
 			"KRP0021_Category module for store home" : ["/html/components/KRP0021.html","","진행중"],
-			"KRP0025_Event list for home" : ["/html/components/KRP0025.html","","진행중"],
+			"KRP0025_event_list_for_category_home" : ["/html/components/KRP0025.html","","진행중"],
 		},
 		"Page" : {
 			"케어솔루션(SLC)" : {
 				"케어솔루션안내" : {
 					"케어플래너" : ["/html/SLC/RMSF2003.html","","진행중"],
+					"케어플래너_총 개수 8개 선택 시도 시" : ["/html/SLC/RMSF-popup01.html","","진행중"],
+					"케어플래너_비로그인 상태에서 [견적 확인하기] 버튼 선택 시" : ["/html/SLC/RMSF-popup02.html","","진행중"],
 				},
 			},
 			"이벤트/멤버십(BMC)" : {
