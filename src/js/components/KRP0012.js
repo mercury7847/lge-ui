c745c3d502f3b3077c23304b782498e7d8155605
pparks <pparks@pparksui-MacBookPro.local>
2020-10-21 11:22:44 +0900

    KRP0012

--- src/js/components/KRP0012.js
+++ src/js/components/KRP0012.js
@@ -1,7 +1,8 @@
 $(window).ready(function(){
     if(!document.querySelector('.KRP0012')) return false;
     
-    vcui.require(['ui/sticky'], function () {        
-        $('.KRP0012').vcSticky();
+    vcui.require(['ui/sticky'], function () {
+        var parentClassName = $('.KRP0012').parent().parent().attr('class');
+        $('.KRP0012').vcSticky({stickyContainer: '.'+parentClassName});
     });
 })
