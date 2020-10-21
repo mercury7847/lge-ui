37c7fae8f79bdbc12df538de2c70b9563ebf8da9
pparks <pparks@pparksui-MacBookPro.local>
2020-10-21 12:48:30 +0900

    KRP0012_2

--- src/js/components/KRP0012.js
+++ src/js/components/KRP0012.js
@@ -2,7 +2,12 @@ $(window).ready(function(){
     if(!document.querySelector('.KRP0012')) return false;
     
     vcui.require(['ui/sticky'], function () {
-        var parentClassName = $('.KRP0012').parent().parent().attr('class');
-        $('.KRP0012').vcSticky({stickyContainer: '.'+parentClassName});
+        var fluid = $('.KRP0012').closest('.container-fluid').length;
+        console.log("fluid : ", fluid);
+        if(fluid){
+            $('.KRP0012').vcSticky({stickyContainer: '.container-fluid'});
+        } else{
+            $('.KRP0012').vcSticky();
+        }        
     });
 })
