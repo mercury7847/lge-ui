cbeaf008c547d82a998448120e307eeafd9e4b5b
pparks <pparks@pparksui-MacBookPro.local>
2020-10-22 14:18:41 +0900

    KRP0018

--- src/js/components/KRP0018.js
+++ src/js/components/KRP0018.js
@@ -17,14 +17,6 @@ $(window).ready(function(){
             '</div>'+
             '<button type="button" class="btn-close"><span class="blind">닫기</span></button>';
 
-        var toastTemplate = 
-            '<div class="toast-message-box">'+
-            '   <div class="inner">'+
-            '       <p class="toast-text">{{message}}</p>'+
-            '       <span class="btn-area"><a href="#" role="button">확인</a></span>'+
-            '   </div>'+
-            '</div>';
-
         function init(){
             $('.btn-init').on('click', function(e){
                 setClearCompare();
@@ -43,12 +35,6 @@ $(window).ready(function(){
                 if($(this).hasClass('close')) openCompareBox();
                 else closeCompareBox();
             });
-
-            $('.toast-message').on('click', 'a[role=button]', function(e){
-                e.preventDefault();
-
-                removeToastAlert();
-            })
 
             setCompares();
             setCompareStatus();
@@ -120,19 +106,8 @@ $(window).ready(function(){
             } else{
                 msg = "비교하기 기능이 초기화되었습니다.";
             }
-
-            var toast = vcui.template(toastTemplate, {message: msg});
-            _$('.toast-message').empty()
-            .html(toast)
-            .css({
-                display:'block', 
-                opacity:0,
-                left: _$(window).width()/2 - _$('.toast-message').width()/2
-            })
-            .transition({opacity:1}, 150, 'easeInOutCubic');
-        }
-        function removeToastAlert(){
-
+            
+            _$(window).trigger("toastshow", msg);
         }
 
         function setClearCompare(){
