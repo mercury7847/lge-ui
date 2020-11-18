6b04cee1708cf65cacde9790745d6c6321249c16
pparks <pparks@pparksui-MacBookPro.local>
2020-11-18 10:56:51 +0900

    care planer

--- src/js/caresolution/carePlaner.js
+++ src/js/caresolution/carePlaner.js
@@ -128,7 +128,7 @@
         '       {{#each item in putitem_list}}'+
         '           <li class="slide-conts ui_carousel_slide">'+
         '               <div class="conts-wrap">'+
-        '                   <div class="prd-care-horizon ui_flexible_box {{item.itemData.serviceName}}">'+
+        '                   <div class="prd-care-horizon ui_flexible_box{{#if item.itemData.combineProduct == "Y"}} comb-type{{/if}}">'+
         '                       <div class="ui_flexible_cont">'+
         '                           <div class="img-wrap">'+
         '                               <img src="{{item.itemData.modelImg}}" alt="{{item.itemData.userFriendlyName}}">'+
@@ -141,7 +141,7 @@
         '                                   <p class="tit"><span class="blind">제품 디스플레이 네임</span>{{item.itemData.userFriendlyName}}</p>'+
         '                                   <p class="code"><span class="blind">제품 코드</span>{{item.itemData.modelName}}</p>'+
         '                               </div>'+
-        '                               <p class="etc">월 {{item.itemData.monthlyPrice}}원<span class="comb-txt"></span></p>'+
+        '                               <p class="etc">월 {{item.itemData.monthlyPrice}}원<span class="comb-txt">{{item.itemData.combineText}}</span></p>'+
         '                           </div>'+  
         '                           <div class="del-item">'+
         '                               <button type="button" class="btn-del" tabindex="" data-put-id="{{item.putID}}"><span class="blind">제품 삭제</span></button>'+
