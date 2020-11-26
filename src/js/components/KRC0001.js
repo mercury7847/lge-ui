51e2c14f9647f79cc0e421ccace37df0467e7725
Jiyeon Lim <jyeonilim@gmail.com>
2020-11-26 10:39:34 +0900

    KRC0001

--- src/js/components/KRC0001.js
+++ src/js/components/KRC0001.js
@@ -18,10 +18,22 @@ $(window).ready(function(){
 						slidesToScroll: 4,
 					}
 				},{
-					breakpoint: 768,
+					breakpoint: 1320,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 3,
+					}
+				},{
+					breakpoint: 1100,
 					settings: {
 						slidesToShow: 2,
 						slidesToScroll: 2,
+					}
+				},{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
 					}
 				}]
 			});
