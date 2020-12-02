0354338d4546661f51d1d9763cecdcf8ac547932
daol <dh.b0926@gmail.com>
2020-12-02 13:02:30 +0900

    Merge branch 'SI-dev' of http://gitlab.lge.co.kr/front/lgekor-ui into SI-dev

--- src/js/support/reservation/addfile.js
+++ src/js/support/reservation/addfile.js
@@ -4,6 +4,10 @@ $('.upload-wrap input[type=file]').change(function(){
     newimage.readAsDataURL(this.files[0]);
     newimage.onload = function(e){
         $('.uploadpreview.' + id ).css('background-image', 'url(' + e.target.result + ')' );
-        $('.uploadpreview.' + id ).closest('.upload-wrap').addClass("on");
+        $('.uploadpreview.' + id ).closest('.upload-wrap').addClass('on');
     }
+});
+
+$('.upload-wrap .btn-ipt-del').click(function(){
+    $(this).closest('.upload-wrap').removeClass('on');
 });
