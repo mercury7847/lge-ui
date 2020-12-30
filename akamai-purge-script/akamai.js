a23d136b0af9c3d831e4a0a5072905d5717cd291
chang0 <chang0@lgcns.com>
2020-12-30 16:11:20 +0900

    akamai.js 로그 수정

--- akamai-purge-script/akamai.js
+++ akamai-purge-script/akamai.js
@@ -7,7 +7,7 @@ var RSYNC_RESULT_FILE = process.env.RSYNC_RESULT_FILE;
 
 
 var rsyncResult = fs.readFileSync(RSYNC_RESULT_FILE, 'UTF-8');
-console.log('===== RSYNC RESULT =====');
+console.log('---------------------------------[ RSYNC RESULT ]----------------------------------');
 console.log(rsyncResult);
 console.log('');
 console.log('');
@@ -39,7 +39,7 @@ lines.forEach(function (line, index) {
     }
 });
 
-console.log('===== PURGE URLS =====');
+console.log('----------------------------------[ PURGE URLS ]-----------------------------------');
 console.log(list);
 console.log('');
 console.log('');
@@ -55,7 +55,7 @@ if (list.length > 0) {
     });
 
     var resBody = res.getBody('utf8');
-    console.log('===== PURGE RESPONSE =====');
+    console.log('--------------------------------[ PURGE RESPONSE ]---------------------------------');
     console.log(JSON.stringify(JSON.parse(resBody), null, 4));
 } else {
     console.log('===== NO PURGE URLS, SKIPP PURGING =====');
