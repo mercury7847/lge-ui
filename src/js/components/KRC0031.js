79ef74ee68544dca6fb25e47df9a2fe6b2df0307
Yun <>
2020-12-30 15:25:50 +0900

    컴포넌트 0029, 0031

--- src/js/components/KRC0031.js
+++ src/js/components/KRC0031.js
@@ -2,4 +2,25 @@ $(window).ready(function(){
     if(!document.querySelector('.KRC0031')) return false;
 
     $('.KRC0031').buildCommonUI();
+
+    vcui.require(['ui/carousel'], function () {
+		$('.KRC0031').find('.ui_carousel_slider').vcCarousel({
+            settings: "unslick",
+            responsive: [
+                {
+                    breakpoint: 10000,
+                    settings: "unslick"
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: false,
+                        dots: true,
+                        slidesToShow: 1, 
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+	});
 })
