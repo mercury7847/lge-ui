0e04e6beea676abbfc2017e643b3583f4b430558
unknown <wardward@cnspartner.com>
2021-01-04 09:34:27 +0900

    CS

--- src/js/support/reservation/serviceEngineer.js
+++ src/js/support/reservation/serviceEngineer.js
@@ -217,6 +217,7 @@
                         }
 
                         $('#stepEnginner').show();
+                        $('.btn-group').show();
                     }
                 });
             });
