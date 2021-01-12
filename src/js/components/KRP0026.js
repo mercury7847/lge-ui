a0eb5c93fe44d0d44a4c32606621ac62cd712f41
정승우 <shiorimate@cnspartner.com>
2021-01-12 15:51:55 +0900

    myEvent

--- src/js/components/KRP0026.js
+++ src/js/components/KRP0026.js
@@ -1,26 +1,70 @@
-$(window).ready(function(){
-    if(!document.querySelector('.KRP0026')) return false;
+(function() {
+    $(window).ready(function(){
+        if(!document.querySelector('.KRP0026')) return false;
 
-    $('.KRP0026').buildCommonUI();
+        $('.KRP0026').buildCommonUI();
 
-    vcui.require(['ui/carousel'], function () {
-        $('.KRP0026').find('.ui_carousel_slider').vcCarousel({
-            settings: "unslick",
-            responsive: [
-                {
-                    breakpoint: 10000,
-                    settings: "unslick"
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        infinite: false,
-                        dots: true,
-                        slidesToShow: 1, 
-                        slidesToScroll: 1
-                    }
-                }
-            ]
-        });
+        var KRP0026 = {
+            init: function() {
+                var self = this;
+
+                vcui.require(['ui/carousel'], function () {
+                    self.setting();
+                    self.bindEvents();
+
+                    //최초 시작시 선택되어 있는 카테고리탭으로 데이타 가져오기
+                    self.requestSelectedCategoryData();
+                });
+            },
+
+            setting: function() {
+                var self = this;
+
+                self.section = $('section.KRP0026');
+
+                self.section.find('.ui_carousel_slider').vcCarousel({
+                    settings: "unslick",
+                    responsive: [
+                        {
+                            breakpoint: 10000,
+                            settings: "unslick"
+                        },
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                infinite: false,
+                                dots: true,
+                                slidesToShow: 1, 
+                                slidesToScroll: 1
+                            }
+                        }
+                    ]
+                });
+
+                var $tab = self.section.find('div.page-header .ui_tab');
+                self.$categoryTab = $tab.eq(0);
+                self.$subCategory = $tab.eq(1);
+            },
+
+            bindEvents: function() {
+                var self = this;
+
+                //카테고리탭
+                self.$categoryTab.on('tabchange', function(e, data) {
+                    var href = $(data.button).attr('href').replace("#", "");
+                    console.log(href);
+                });
+            },
+
+            requestSelectedCategoryData: function() {
+                var self = this;
+                var $a = self.$categoryTab.find('li.on a');
+                var href = $a.attr('href').replace("#", "");
+                console.log($a, href);
+            }
+
+        };
+
+        KRP0026.init();
     });
-});
+})();
