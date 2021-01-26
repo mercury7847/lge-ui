cbf1210c36c24e0ad4961fb03cddc7908c0e32d8
unknown <wardward@cnspartner.com>
2021-01-26 11:12:11 +0900

    CS

--- src/js/support/reservation/requestCall.js
+++ src/js/support/reservation/requestCall.js
@@ -131,9 +131,8 @@
 
                 $('#route').val(lgkorUI.isMobile() ? 'WWW2' : 'WWWW1');
 
-                self.$cont.commonModel({
-                    register: register
-                });
+                self.bindEvent();
+
                 self.$calendarDate.calendar({
                     inputTarget: '#date'
                 });
@@ -141,17 +140,21 @@
                     inputTarget: '#time'
                 });
 
-                self.bindEvent();
+                self.$cont.commonModel({
+                    register: register,
+                    selected: {
+                        category: self.$cont.find('#category').val(),
+                        categoryName: self.$cont.find('#categoryNm').val(),
+                        subCategory: self.$cont.find('#subCategory').val(),
+                        subCategoryName: self.$cont.find('#subCategoryNm').val(),
+                        modelCode: self.$cont.find('#modelCode').val(),
+                        productCode: self.$cont.find('#productCode').val()
+                    }
+                });
             });
         },
         completeModel: function(url) {
-            var self = this;
-
-            if (self.model.isRequest) {
-                self.setInputStep(url);
-            } else {
-                self.nextInputStep();
-            }
+            this.setInputStep(url);
         },
         nextInputStep: function() {
             var self = this;
