e69fba29d3abae2d270e45b1016bbdf43671dbe0
unknown <wardward@cnspartner.com>
2021-01-27 10:50:29 +0900

    CS

--- src/js/support/appgate.js
+++ src/js/support/appgate.js
@@ -1,7 +1,12 @@
 (function() {
     var param;
 
-    $(window).ready(function() {
+    $(window).ready(function() {   
+        var cookieVal = $('.product-info').find('.name em').text().split('.')[0];
+        var cookie = lgkorUI.recentlySearch;
+
+        cookie.addCookie(cookieVal);
+
         vcui.require([
             'ui/validation', 'ui/selectTarget'
         ], function() {
