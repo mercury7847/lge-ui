7ec8b5199d1ae7a084c976f2903a806c93f46d7d
LGE\jaekwan.bae <jaekwan.bae>
2021-02-05 19:09:23 +0900

    소모품 제품 리스트 - 소모품 사용 가능 모델 검색

--- src/js/support/consumables.js
+++ src/js/support/consumables.js
@@ -1,6 +1,62 @@
-(function() {
+$(window).ready(function() {
+    //구매 수량 선택
+    $('.quantity-wrap').on('click', 'button.minus,button.plus',function (e) {
+        var $input = $(this).siblings('input');
+        var quantity = $input.val();
+        if($(this).hasClass('minus')) {
+            --quantity;
+            if(quantity < 1) {
+                quantity = 1;
+            }
+            
+            if(quantity == 1) {
+                $(this).attr('disabled',true);
+            } else {
+                $(this).removeAttr('disabled');
+            }
+        } else if($(this).hasClass('plus')) {
+            ++quantity;
+
+            if(quantity > 1) {
+                $(this).siblings('button.minus').removeAttr('disabled');
+            }
+        }
+        $input.val(quantity);
+    });
     
+    $(".search-input .btn-delete").click(function() {
+        var temp_ul = $(this).parents('.model-infomation').find('.model-infomation-list');
+        var temp_li = $(temp_ul).find('li');
+        var $nodata = $('.no-data');
 
+        $(this).siblings("#keyword").val("");
+        temp_ul.show();
+        temp_li.show();
+        $nodata.hide();
+    });
 
-    console.log('고객지원 소모품 페이지');
-})();
+    $("#keyword").on("input", function() {
+        var k = $(this).val();
+
+        var temp_ul = $(this).parents('.model-infomation').find('.model-infomation-list');
+        var temp_li = $(temp_ul).find('li');
+        // var temp_visible = $(temp_ul).find('li:visible');
+        var $nodata = $('.no-data');
+
+        $(temp_li).hide();
+        $(temp_ul).find("li:contains('" + k + "')").show();
+
+        if( k == "") {
+            temp_ul.show();
+            $nodata.hide();
+        } else {
+            if( !$(temp_ul).find("li:contains('" + k + "')").length) {
+                temp_ul.hide();
+                $nodata.show();
+            } else { 
+                temp_ul.show()
+                $nodata.hide();
+            }
+        }
+    });
+});
