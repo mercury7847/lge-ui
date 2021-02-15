fd96c6c84d011e2fb84dfb7750905a890ad65f8e
unknown <wardward@cnspartner.com>
2021-02-15 16:54:11 +0900

    CS 예약 솔루션

--- src/js/support/reservation/serviceEngineer.js
+++ src/js/support/reservation/serviceEngineer.js
@@ -291,12 +291,14 @@
                 }
             });
         },
-        setSolutions: function(url, param, isShown) {
+        setSolutions: function(param, isShown) {
             var self = this;
+            var url = self.$solutionsPopup.data('listUrl');
 
             lgkorUI.requestAjaxData(url, param, function(result){
                 self.$solutionsPopup.find('.pop-conts').html(result);
                 self.$solutionsPopup.find('.pagination').pagination();
+                
                 if (isShown) {
                     self.$solutionsPopup.find('.ui_accordion').vcAccordion();
                 } else {
@@ -304,15 +306,14 @@
                 }
 
                 self.$solutionsPopup.find('.pagination').on('pageClick', function(e) {
-                    var url = self.$solutionsPopup.data('listUrl'),
-                        param = {
+                    var param = {
                             topic : $('input[name=topic]:checked').val(),
                             subToic : $('input[name=subTopic]:checked').val(),
                             productCode : $('#productCode').val(),
                             page: e.page
                         };
 
-                    self.setSolutions(url, param, true);
+                    self.setSolutions(param, true);
                 });
             }, null, "html", true);
         },
@@ -534,10 +535,16 @@
                 var notInput = '';
                 if (self.isLogin) {
                     notInput += '#userNm, #phoneNo';
-                    if ($('#detailAddress').prop('readonly')) {
+                    if ($('#detailAddress').data('defaultValue')) {
                         notInput += ', #zipCode, #userAddress, #detailAddress';
+                        $('[data-default-value]').each(function() {
+                            $(this).val($(this).data('defaultValue')).prop('readonly', true);
+                        });
                     }
+                    $('.address-err-msg + .chk-wrap').hide();
+                    $('.address-err-msg + .chk-wrap').find('input[type=radio]').prop('checked', false);
                 }
+
                 $('input[type=text], textarea').not(notInput).val('');
 
                 $('#fanEtc').prop('disabled', true);
@@ -672,7 +679,6 @@
 
             // 솔루션 배너
             self.$solutionsBanner.find('.btn-link').on('click', function(){
-                var url = $(this).data('href');
                 var param = {
                     topic : $('input[name=topic]:checked').val(),
                     subToic : $('input[name=subTopic]:checked').val(),
@@ -680,7 +686,7 @@
                     page: 1
                 };   
 
-                self.setSolutions(url, param, false);
+                self.setSolutions(param, false);
             });
 
             // 주소 찾기
