0fcd0532381a00b8094e82a0038bb78ed1ead1b8
unknown <wardward@cnspartner.com>
2021-02-17 12:49:13 +0900

    cs

--- src/js/support-common.js
+++ src/js/support-common.js
@@ -777,15 +777,6 @@ CS.MD.commonModel = function() {
                     data = $this.data();
                     opt = self.options;
 
-                self.param = $.extend(self.param, {
-                    keyword: '',
-                    category: data.category,
-                    categoryNm: data.categoryName,
-                    subCategory: data.subCategory,
-                    subCategoryNm: data.subCategoryName,
-                    page: 1
-                });
-
                 if (self.$el.hasClass('service-engineer') && data.subCategory == 'CT50019275') {
                     lgkorUI.confirm('의류 건조기 제품은 불편 사항 및 제품 환경 확인 등이 필요 함에 따라 고객 상담실 1544-7777로 전화주시면 <br>신속한 상담에 도움드리고 있습니다.<br><br>업무 시간 외, 공휴일, 상담사 통화가 어려운 경우 아래 ’예약’ 버튼을 클릭하시어 연락처 등을 남겨 주시기 바랍니다. <br>다만, 접수된 순으로 처리하고 있어 다소 지연되는 점 양해 부탁드립니다.',{
                         typeClass:'type2',
@@ -801,6 +792,15 @@ CS.MD.commonModel = function() {
                     });
                     return;
                 }
+
+                self.param = $.extend(self.param, {
+                    keyword: '',
+                    category: data.category,
+                    categoryNm: data.categoryName,
+                    subCategory: data.subCategory,
+                    subCategoryNm: data.subCategoryName,
+                    page: 1
+                });
 
                 self.updateSummary({
                     product: [data.categoryName, data.subCategoryName]
@@ -861,6 +861,22 @@ CS.MD.commonModel = function() {
             self.$modelBox.find('#subCategorySelect').on('change', function() {
                 var $this = $(this),
                     $category = self.$modelBox.find('#categorySelect');
+
+                if (self.$el.hasClass('service-engineer') && $this.val() == 'CT50019275') {
+                    lgkorUI.confirm('의류 건조기 제품은 불편 사항 및 제품 환경 확인 등이 필요 함에 따라 고객 상담실 1544-7777로 전화주시면 <br>신속한 상담에 도움드리고 있습니다.<br><br>업무 시간 외, 공휴일, 상담사 통화가 어려운 경우 아래 ’예약’ 버튼을 클릭하시어 연락처 등을 남겨 주시기 바랍니다. <br>다만, 접수된 순으로 처리하고 있어 다소 지연되는 점 양해 부탁드립니다.',{
+                        typeClass:'type2',
+                        title:'',
+                        okBtnName: '예약',
+                        cancelBtnName: '이전',
+                        ok: function() {
+                            location.href = '/support/request-call-reservation-dryer';
+                        },
+                        cancel: function() {
+                            self.reset();
+                        }
+                    });
+                    return;
+                }
 
                 self.param = $.extend(self.param, {
                     category: $category.val(),
