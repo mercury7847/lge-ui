5b7611b23d9a0732fa160901680e6f7c9a4f7c0c
unknown <wardward@cnspartner.com>
2021-02-25 10:48:59 +0900

    CS

--- src/js/support-common.js
+++ src/js/support-common.js
@@ -2113,68 +2113,6 @@ CS.MD.quickMenu = function() {
     CS.MD.plugin(pluginName, Plugin);
 }();
 
-/*
-* survey
-*/
-CS.MD.survey = function(addData) {
-    if ($('#surveyPopup').length < 1) return false;
-
-    vcui.require(['ui/validation'], function () {
-        var $cont = $('.survey-pop');
-        var register = {
-            privcyCheck: {
-                msgTarget: '.err-block'
-            },
-            userName: {
-                msgTarget: '.err-block'
-            },
-            phoneNo: {
-                msgTarget: '.err-block'
-            },
-            gender: {
-                msgTarget: '.gender-err-block'
-            },
-            age: {
-                msgTarget: '.err-block'
-            },
-            serviceName: {
-                msgTarget: '.service-err-block'
-            },
-            csaFlag: {
-                msgTarget: '.csa-err-block'
-            },
-            rating: {
-                msgTarget: '.err-block'
-            },
-            content: {
-                msgTarget: '.err-msg'
-            }
-        };
-        var validation = new vcui.ui.CsValidation('.survey-pop', {register:register});
-
-        $('#surveyPopup').find('.btn-confirm').on('click', function() {
-            var result = validation.validate(),
-                data = $.extend({}, addData, validation.getAllValues());
-
-            if (result.success) {
-                lgkorUI.requestAjaxDataPost($cont.data('ajax'), data, function(result) {
-                    if (result.data.resultFlag == 'Y') {
-                        $('#surveyPopup').vcModal('hide');
-                    }
-
-                    lgkorUI.alert("", {
-                        title: result.data.resultMessage
-                    });
-                })
-            }
-        });
-
-        $('#surveyPopup').on('modalhide', function(e) {
-            $cont.find('input,textarea').not(':readonly').val('');
-        });
-    });
-};
-
 var AuthManager = function() {
     var SENDTEXT = '인증번호 발송';
     var RESENDTEXT = '인증번호 재발송';
