979a26c82109ba28e01feef4f3fa60e9444ae675
정승우 <shiorimate@cnspartner.com>
2021-02-25 21:35:03 +0900

    KRP0012

--- src/js/components/KRP0008.js
+++ src/js/components/KRP0008.js
@@ -1371,3 +1371,26 @@
         KRP0008.init();
     });
 })();
+
+//크레마
+window.cremaAsyncInit = function () {
+    crema.init(null,null);
+};
+
+(function(i,s,o,g,r,a,m){
+    if(s.getElementById(g)){
+        return
+    };
+    a=s.createElement(o),m=s.getElementsByTagName(o)[0];
+    a.id=g;
+    a.async=1;
+    a.src=r;
+    m.parentNode.insertBefore(a,m);
+    console.log('i',i);
+    console.log('s',s);
+    console.log('o',o);
+    console.log('g',g);
+    console.log('r',r);
+    console.log('a',a);
+    console.log('m',m);
+})(window,document,'script','cremajssdk','//widgets.cre.ma/lge.co.kr/init.js');
