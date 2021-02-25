abae1e00d8699c183852e7ab78f67e23ba1fc3f8
duil Kim <duil2121.kim@cnspartner.com>
2021-02-25 21:08:14 +0900

    add

--- src/js/components/KRC0021.js
+++ src/js/components/KRC0021.js
@@ -4,7 +4,14 @@ $(window).ready(function(){
     $('.KRC0021').buildCommonUI();
 
     vcui.require(['ui/carousel'], function () {
-        $('.KRC0021').find('.ui_carousel_slider').vcCarousel({
+        $('.KRC0021').find('.ui_carousel_slider').on('carouselinit',function(e, carousel, data){
+
+            vcui.require(['ui/videoBox','ui/youtubeBox'], function(){
+                $('.KRC0021').find('.animation-box').vcVideoBox();
+                $('.KRC0021').find('.youtube-box').vcYoutubeBox();
+            });            
+
+        }).vcCarousel({
             responsive: [
                 {
                     breakpoint: 10000,
