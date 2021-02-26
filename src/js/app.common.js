635629a227e327041c123a53388bbd0fcbcca02d
hoeseob.lee <hoeseob.lee@cnspartner.com>
2021-02-26 14:14:00 +0900

    앱 옵션 스크립트 수정

--- src/js/app.common.js
+++ src/js/app.common.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    //헤더 앱 설정 버튼
     if (isApp()) {
+        //헤더 앱 설정 버튼
         $('.mapExclusive').addClass('active');
         $('.mapExclusiveDss').hide();
         $(".app-settings-button").on({
@@ -10,5 +10,9 @@ $(document).ready(function(){
         });
         //Quick메뉴 삭제
         $('.KRP0005,.KRP0032,.quick-menu-list').remove();
+        //Quick메뉴 AR 버튼 추가
+        $('.KRP0004').parent("div").prepend('<div class="floating-menu cs-cst btn-app-ar"><div class="app-ar"><a href="#"><span>AR</span></a></div></div>');
+        //제조번호 카메라 버튼 노출
+        //$('#appType').addClass("app-type");
     }
 });
