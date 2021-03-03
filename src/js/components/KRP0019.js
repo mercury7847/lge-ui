6f0105a387db51043e129cacbdb985ccebf75709
정승우 <shiorimate@cnspartner.com>
2021-03-03 16:58:28 +0900

    krp

--- src/js/components/KRP0019.js
+++ src/js/components/KRP0019.js
@@ -1,3 +1,61 @@
+//크레마
+(function(i,s,o,g,r,a,m){
+    if(s.getElementById(g)){
+        return
+    };
+    a=s.createElement(o),m=s.getElementsByTagName(o)[0];
+    a.id=g;
+    a.async=1;
+    a.src=r;
+    m.parentNode.insertBefore(a,m);
+    /*
+    console.log('i',i);
+    console.log('s',s);
+    console.log('o',o);
+    console.log('g',g);
+    console.log('r',r);
+    console.log('a',a);
+    console.log('m',m);
+    */
+})(window,document,'script','cremajssdk','//widgets.cre.ma/lge.co.kr/init.js');
+
+
+(function() {
+    var KRP0012 = {
+        init: function() {
+            var self = this;
+            if(typeof digitalData !== 'undefined') {
+                if(digitalData && !vcui.isEmpty(digitalData)) {
+                    window.cremaAsyncInit = function () {
+                        crema.init("이름",digitalData.unifyId);
+                    };
+                } else {
+                    window.cremaAsyncInit = function () {
+                        crema.init(null,null);
+                    };
+                }
+            } else {
+                window.cremaAsyncInit = function () {
+                    crema.init(null,null);
+                };
+            }
+
+            var $section = $('.KRP0019');
+            var widgetId = $section.data('widgetId');
+            if(widgetId) {
+                $section.find('.cont-wrap').html('<div class="crema-reviews" data-widget-id="'  + widgetId + '"></div>');
+            }
+        }
+    }
+
+    $(window).ready(function(){
+        if(!document.querySelector('.KRP0019')) return false;
+        //$('.KRP0019').buildCommonUI();
+        KRP0012.init();
+    });
+})();
+
+/*
 $(window).ready(function(){
 	if(!document.querySelector('.KRP0019')) return false;
 
@@ -16,3 +74,4 @@ $(window).ready(function(){
 		});
 	});
 })
+*/
