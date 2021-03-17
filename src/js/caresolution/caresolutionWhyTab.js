7331ee1022afeccf25f95ddf0def80a13e05435e
duil Kim <duil2121.kim@cnspartner.com>
2021-03-17 16:11:00 +0900

    add

---
+++ src/js/caresolution/caresolutionWhyTab.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    vcui.require(['ui/tab','ui/sticky'], function () {
+
+        $('.ui_why_tab').on('tabchange', function(e,data){
+
+            var $content = $(data.content);
+            var stickyHeight = $content.closest('.ui_sticky').height();
+            var top = $content.offset().top - parseInt($content.css('padding-top'));
+            $('html, body').scrollTop(top-stickyHeight);
+
+        }).vcTab();
+    })
+});
