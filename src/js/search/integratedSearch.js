050e94505d33a9577a8f769e094ae8284fb6aeb9
정승우 <shiorimate@cnspartner.com>
2021-03-24 14:42:39 +0900

    search

--- src/js/search/integratedSearch.js
+++ src/js/search/integratedSearch.js
@@ -119,11 +119,13 @@
                 self.$searchLayer.vcModal();
             });
             */
+           /*
            $('a[href="#layerSearch"]').on('click', function(e){
                console.log('layter but');
                self.$inputSearch.focus();
                self.$inputSearch.click();
            });
+           */
 
             self.$searchLayer.off('modalshown').on('modalshown', function(e, data){
                 console.log('modalSHown');
