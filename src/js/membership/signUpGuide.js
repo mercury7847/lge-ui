7d03886fb0c2dd9becd2243f9da1f14889287a72
duil Kim <duil2121.kim@cnspartner.com>
2021-03-30 16:44:11 +0900

    add

--- src/js/membership/signUpGuide.js
+++ src/js/membership/signUpGuide.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
         $content.find('.device-aos-qrcode').show();
         $content.find('.device-ios-qrcode').hide();
     }
-
     if(isIOS){ 
         $content.find('.device-aos-qrcode').hide();
         $content.find('.device-ios-qrcode').show();
