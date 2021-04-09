23d61842420a8957f2929e8a30274c9cdb93a79f
정승우 <shiorimate@cnspartner.com>
2021-04-09 20:00:30 +0900

    검색 자동완성 기능 실행 최소 글자수 1로 수정

--- src/js/lg.common.js
+++ src/js/lg.common.js
@@ -277,7 +277,7 @@ var isApp = function(){
         INTERGRATED_SEARCH_VALUE: "intergratedSearchValue",
         MAX_SAVE_RECENT_KEYWORD: 5, //최근 검색어 저장 최대수
         MAX_SAVE_RECENT_PRODUCT: 10, //최근 본 제품 저장 최대수,
-        SEARCH_AUTOCOMPLETE_MIN_LENGTH: 2, // 검색 자동 완성 기능 실행 최소 글자수
+        SEARCH_AUTOCOMPLETE_MIN_LENGTH: 1, // 검색 자동 완성 기능 실행 최소 글자수
         SEARCH_AUTOCOMPLETE_TIMER: 300, // 검색 자동 완성 기능 키보드 클릭 타이머
         DOMAIN_LIST:["www.lge.co.kr", 'wwwstg.lge.co.kr', 'wwwdev50.log.co.kr'],
         init: function(){
