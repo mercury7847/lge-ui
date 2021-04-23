0126448dc4b434b24daa00748dfc78d1c93551a2
unknown <wardward@cnspartner.com>
2021-04-23 10:29:22 +0900

    CS

--- src/js/support/customVoice/myStatus.js
+++ src/js/support/customVoice/myStatus.js
@@ -22,46 +22,28 @@
                     }
                 }
             });
-
-            var authRegister = {
-                userName2: {
-                    required: true,
-                    maxLength: 30,
-                    pattern: /^[가-힣\s]+$|^[a-zA-Z\s]+$/,
-                    msgTarget: '.err-block',
-                    errorMsg: '이름을 입력해 주세요.',
-                    patternMsg: '이름은 한글 또는 영문으로만 입력해주세요.'
-                },
-                phoneNo: {
-                    required: true,
-                    minLength: 10,
-                    maxLength: 11,
-                    msgTarget: '.err-block',
-                    errorMsg: '정확한 휴대폰번호를 입력해주세요.',
-                    patternMsg: '정확한 휴대폰번호를 입력해주세요.',
-                    validate : function(value){
-                        return validatePhone(value);
-                    } 
-                },
-                authNo:{
-                    required: true,
-                    msgTarget: '.err-block',
-                    errorMsg: '인증번호를 입력해주세요.',
-                }
-            };
-
-            if( $('#phoneForm').length ) { 
-                self.authManager = new AuthManager({
-                    elem: {
-                        form: '#phoneForm',
-                        name: '#userName2',
-                        phone: '#phoneNo',
-                        number: '#authNo'
+            var phoneValidation = new vcui.ui.CsValidation('#phoneForm', {
+                register: {
+                    userName2: {
+                        msgTarget: '.err-block',
+                        pattern: /^[가-힣\s]|[a-zA-Z\s]+$/,
+                        maxLength: 30
                     },
-                    register: authRegister
-                });
-
-            }
+                    phoneNo: {
+                        minLength: 10,
+                        maxLength: 11,
+                        msgTarget: '.err-block',
+                        errorMsg: '정확한 휴대폰번호를 입력해주세요.',
+                        patternMsg: '정확한 휴대폰번호를 입력해주세요.',
+                        validate : function(value){
+                            return validatePhone(value);
+                        } 
+                    },
+                    authNo: {
+                        msgTarget: '.err-block',
+                    }
+                }
+            });
 
 
             $('#numberForm').find('.btn-confirm').on('click', function() {
@@ -87,13 +69,50 @@
             });
 
             $('#phoneForm').find('.btn-confirm').on('click', function() {
-                self.authManager.confirm(this, function(success, result) {
-                    //success && self.complete();
-                });
+                var result = phoneValidation.validate(),
+                    data = phoneValidation.getAllValues();
+
+                if (result.success) {
+                    if ($('#authNo').prop('disabled')) {
+                        $('#laypop1').vcModal(); 
+                        return false;
+                    }
+
+                    lgkorUI.showLoading();
+                    lgkorUI.requestAjaxDataPost($('#numberForm').data('ajax'), data, function(result) {
+
+                        if (result.data.resultFlag == 'Y') {
+                            $('#phoneForm').submit();
+                        } else if (result.data.resultFlag == 'N') {
+                            lgkorUI.alert("", {
+                                title: result.data.resultMessage
+                            });
+                        }
+
+                        lgkorUI.hideLoading();
+                    });
+                }
             });
 
             $('.btn-send').on('click', function() {
-                self.authManager.send(this);
+                var result = phoneValidation.validate(['userName2', 'phoneNo']),
+                    ajaxUrl = $(this).data('ajax'),
+                    data = phoneValidation.getValues(['userName2','phoneNo']);
+
+                if (result.success) {
+                    lgkorUI.requestAjaxDataPost(ajaxUrl, data, function(result) {
+                        var data = result.data;
+    
+                        if (data.resultFlag == 'Y') {
+                            $(this).html('인증 번호 재발송');
+                            $('#authNo').prop('disabled', false);
+                        }
+
+                        lgkorUI.alert("", {
+                            title: result.data.resultMessage
+                        });
+                    });
+                }
             });
         });
     });
