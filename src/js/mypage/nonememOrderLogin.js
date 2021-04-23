53028194723b74d0b8d0731846a0a53ffc3b2fb9
pparks <pparks@pparksui-MacBookPro.local>
2021-04-23 16:38:44 +0900

    none member order

--- src/js/mypage/nonememOrderLogin.js
+++ src/js/mypage/nonememOrderLogin.js
@@ -83,6 +83,12 @@
         .on('tabchange', function(e, data){
             $('.err-block').hide();
         });
+
+        if(!vcui.detect.isMobileDevice){
+            $('#popup-nodata').find('.pop-conts a').on('click', function(e){
+                e.preventDefault();
+            })
+        }
     }
 
     function loginChecked(){
