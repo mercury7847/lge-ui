33427ebb4e07c3c19ccd3aec43c47dc84a7867fa
pparks <pparks@pparksui-MacBookPro.local>
2021-04-28 16:16:39 +0900

    request rental

--- src/js/caresolution/requestRental.js
+++ src/js/caresolution/requestRental.js
@@ -55,14 +55,13 @@
 
     function init(){    
         vcui.require(['ui/checkboxAllChecker', 'ui/accordion', 'ui/modal', 'ui/validation', 'ui/calendar'], function () {
-
             if(vcui.detect.isIOS){
                 isProgress = false;
 
                 $('input[name=chkPrivacy], input[name=rentalAgree]').prop('disabled', true);
 
                 lgkorUI.alert("", {
-                    title: "죄송합니다.<br>현재 iOS에서는 케어솔루션/케어십 청약 서비스를 제공하지 않습니다. <br>케어솔루션 고객센터로 연락주시면 친절하게<br>안내 드리겠습니다.<br>케어솔루션 고객센터 전화하기<br>(전화)<a href='tel:1544-6351'>1544-6351</a>",
+                    title: "죄송합니다.<br>현재 iOS에서는 케어솔루션/케어십 청약 서비스를 제공하지 않습니다. <br>케어솔루션 고객센터로 연락주시면<br>친절하게 안내 드리겠습니다.<br>케어솔루션 고객센터 전화하기<br>(전화)<a href='tel:1544-6351'>1544-6351</a>",
                     ok: function(){
                         location.href = "/";
                     }
