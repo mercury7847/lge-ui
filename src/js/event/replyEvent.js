4f86abc6b1f4cc6aa10c39d58dcbdf68b3c0f099
방휘랑 <bhl20@cnspartner.com>
2021-05-10 12:40:46 +0900

    응원댓글 이벤트 JS 임시 수정

--- src/js/event/replyEvent.js
+++ src/js/event/replyEvent.js
@@ -47,33 +47,33 @@
             self.$btnWrap.on('click','a.write',function (e) {
                 e.preventDefault();
 
-                if(self.$wrap.data('eventRestrictFlag') == "Y") {
-                    lgkorUI.alert("", {title: "서버 점검중입니다."});
-                    return;
-                }
+                if($('html').hasClass('app')) {
+                    if(self.$wrap.data('eventRestrictFlag') == "Y") {
+                        lgkorUI.alert("", {title: "서버 점검중입니다."});
+                        return;
+                    }
+    
+                    if(lgkorUI.stringToBool($(this).data('isToday'))) {
+                        //금일 이미 참여한 이벤트
+                        lgkorUI.alert("", {title: '하루에 한번만 참여 가능합니다.'});
+                        return;
+                    }
+                    //로그인을 해야 하는가
+                    var login = self.$wrap.data('loginUrl');
+                    if(login && login.length > 0) {
+                        var obj = {title:'로그인이 필요합니다.<br>이동하시겠습니까', cancelBtnName:'아니오', okBtnName:'네', ok: function (){
+                            location.href = login;
+                        }};
+                        lgkorUI.confirm(null, obj);
+                        return;
+                    }
 
-                if(lgkorUI.stringToBool($(this).data('isToday'))) {
-                    //금일 이미 참여한 이벤트
-                    lgkorUI.alert("", {title: '하루에 한번만 참여 가능합니다.'});
-                    return;
+                    self.$replyPopup.find('#reply').val("");
+                    self.$replyPopup.find('input[type="checkbox"]').prop('checked',false);
+                    self.$replyPopup.vcModal({opener: this});
+                } else {
+                    lgkorUI.alert("", {title:"본 이벤트 참여는 모바일 앱에서만 가능합니다.<br>구글 플레이/앱 스토어에서<br>'LGE.COM'을 검색하여<br>앱 다운로드 후 참여해주시기 바랍니다.<br>감사합니다."});
                 }
-                //로그인을 해야 하는가
-                var login = self.$wrap.data('loginUrl');
-                if(login && login.length > 0) {
-                    var obj = {title:'로그인이 필요합니다.<br>이동하시겠습니까', cancelBtnName:'아니오', okBtnName:'네', ok: function (){
-                        location.href = login;
-                    }};
-                    lgkorUI.confirm(null, obj);
-                    return;
-                }
-
-                self.$replyPopup.find('#reply').val("");
-                self.$replyPopup.find('input[type="checkbox"]').prop('checked',false);
-                self.$replyPopup.vcModal({opener: this});
-                // if($('html').hasClass('app')) {
-                // } else {
-                //     lgkorUI.alert("", {title:"본 이벤트 참여는 모바일 앱에서만 가능합니다.<br>구글 플레이/앱 스토어에서<br>'LGE.COM'을 검색하여<br>앱 다운로드 후 참여해주시기 바랍니다.<br>감사합니다."});
-                // }
             });
             
 
@@ -81,49 +81,49 @@
             self.$btnWrap.on('click','a.result',function (e) {
                 e.preventDefault();
 
-                if(self.$wrap.data('eventRestrictFlag') == "Y") {
-                    lgkorUI.alert("", {title: "서버 점검중입니다."});
-                    return;
-                }
-
-                //로그인을 해야 하는가
-                var login = self.$wrap.data('loginUrl');
-                if(login && login.length > 0) {
-                    var obj = {title:'로그인이 필요합니다.<br>이동하시겠습니까', cancelBtnName:'아니오', okBtnName:'네', ok: function (){
-                        location.href = login;
-                    }};
-                    lgkorUI.confirm(null, obj);
-                    return;
-                }
-
-                var param = {};
-                var eventId = self.$wrap.data('eventId');
-                param.eventId = eventId;
-                param.userPhone = self.$phone.val();
-
-                var ajaxUrl = self.$wrap.attr('data-check-url');
-                lgkorUI.showLoading();
-                lgkorUI.requestAjaxData(ajaxUrl,param,function(result) {
-                    var data = result.data;
-                    if(lgkorUI.stringToBool(data.win)) {
-                        var template = '<strong class="tit">축하드립니다!</strong>' +
-                            '<span class="txt">{{#raw prizeWin}}</span>' +
-                            '<span class="txt">사은품은 {{date}} 이후 참여하신<br>휴대폰 번호로 모바일 쿠폰 지급 예정입니다.</span>';
-                        $('#popupEventWin').find('p.desc').html(vcui.template(template, data));
-                        $('#popupEventWin').vcModal({opener: this});
-                    } else {
-                        var template = '<strong class="tit">아쉽습니다.</strong>' +
-                            '<span class="txt">{{#raw prizeLose}}</span>' +
-                            '<span class="txt">다음 이벤트에 참여하여 다시 한번 도전해보세요!</span>';
-                        $('#popupEventLose').find('p.desc').html(vcui.template(template, data));
-                        $('#popupEventLose').vcModal({opener: this});
+                if($('html').hasClass('app')) {
+                    if(self.$wrap.data('eventRestrictFlag') == "Y") {
+                        lgkorUI.alert("", {title: "서버 점검중입니다."});
+                        return;
                     }
-                    self.$btns.removeClass('apply');
-                });
-                // if($('html').hasClass('app')) {
-                // } else {
-                //     lgkorUI.alert("", {title:"본 이벤트 참여는 모바일 앱에서만 가능합니다.<br>구글 플레이/앱 스토어에서<br>'LGE.COM'을 검색하여<br>앱 다운로드 후 참여해주시기 바랍니다.<br>감사합니다."});
-                // };
+    
+                    //로그인을 해야 하는가
+                    var login = self.$wrap.data('loginUrl');
+                    if(login && login.length > 0) {
+                        var obj = {title:'로그인이 필요합니다.<br>이동하시겠습니까', cancelBtnName:'아니오', okBtnName:'네', ok: function (){
+                            location.href = login;
+                        }};
+                        lgkorUI.confirm(null, obj);
+                        return;
+                    }
+    
+                    var param = {};
+                    var eventId = self.$wrap.data('eventId');
+                    param.eventId = eventId;
+                    param.userPhone = self.$phone.val();
+    
+                    var ajaxUrl = self.$wrap.attr('data-check-url');
+                    lgkorUI.showLoading();
+                    lgkorUI.requestAjaxData(ajaxUrl,param,function(result) {
+                        var data = result.data;
+                        if(lgkorUI.stringToBool(data.win)) {
+                            var template = '<strong class="tit">축하드립니다!</strong>' +
+                                '<span class="txt">{{#raw prizeWin}}</span>' +
+                                '<span class="txt">사은품은 {{date}} 이후 참여하신<br>휴대폰 번호로 모바일 쿠폰 지급 예정입니다.</span>';
+                            $('#popupEventWin').find('p.desc').html(vcui.template(template, data));
+                            $('#popupEventWin').vcModal({opener: this});
+                        } else {
+                            var template = '<strong class="tit">아쉽습니다.</strong>' +
+                                '<span class="txt">{{#raw prizeLose}}</span>' +
+                                '<span class="txt">다음 이벤트에 참여하여 다시 한번 도전해보세요!</span>';
+                            $('#popupEventLose').find('p.desc').html(vcui.template(template, data));
+                            $('#popupEventLose').vcModal({opener: this});
+                        }
+                        self.$btns.removeClass('apply');
+                    });
+                } else {
+                    lgkorUI.alert("", {title:"본 이벤트 참여는 모바일 앱에서만 가능합니다.<br>구글 플레이/앱 스토어에서<br>'LGE.COM'을 검색하여<br>앱 다운로드 후 참여해주시기 바랍니다.<br>감사합니다."});
+                };
             });
 
             //팝업 댓글쓰기
