bd39310c7bd884e9c3e1f1b7b1a15a1f4231aba6
최주영 <hani@cnspartner.com>
2021-05-25 15:16:03 +0900

    [UI]BTOCSITE-186_1 - 스토리 > 홈 구독유도 영역 위치 변경

--- src/js/home/storyHome.js
+++ src/js/home/storyHome.js
@@ -1,5 +1,4 @@
 ;(function(){
-
     var stickyTagTemplate = 
         '<div class="subscribe-wrap ui_sticky">'+
             '<div class="inner">'+
@@ -404,6 +403,7 @@
             // console.log("result.data.selectTags:", result.data.selectTags);
             var viewMode;
             if(result.data.selectTags){
+
                 viewMode = "selectTagMode";
                 
                 sectionItem.find('.inner h2.title').hide();
@@ -437,7 +437,7 @@
                     sectionItem.find('.flexbox-wrap').append(list);
                 }
 
-                if(page == 1){
+                if(page == 1){ 
                     if(IS_LOGIN == "Y"){
                         if(viewMode == "listMode" && sectioname == "user_story"){
                             $('.tag-subscribe-story').empty().hide();
@@ -448,7 +448,10 @@
                         }
                     } else{
                         if(sectioname == "new_story"){
-                            $('.tag-subscribe-story').empty().show().append(vcui.template(recommendTagTemplate, {tagList:result.data.recommendTags}));
+                            //$('.tag-subscribe-story').empty().show().append(vcui.template(recommendTagTemplate, {tagList:result.data.recommendTags}));
+                            /* 20210518 추가 */
+                            $('.tag-subscribe-story2').empty().show().append(vcui.template(recommendTagTemplate, {tagList:result.data.recommendTags}));
+                            /* //20210518 추가 */
                             $('.ui_tag_smooth_scrolltab').vcSmoothScrollTab();
                         }
                     }
@@ -480,7 +483,10 @@
                 setRepositionTagBox(sectionItem);
             } else{
                 if(sectioname == "user_story"){
+                    // $('.tag-subscribe-story').empty().show().append(vcui.template(recommendTagTemplate, {tagList:result.data.recommendTags}));
+                    /* 20210518 추가 */    
                     $('.tag-subscribe-story').empty().show().append(vcui.template(recommendTagTemplate, {tagList:result.data.recommendTags}));
+                    /* //20210518 추가 */
                     $('.ui_tag_smooth_scrolltab').vcSmoothScrollTab();
 
                     $('.new_story').find('.inner h2.title').hide();
