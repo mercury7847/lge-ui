7ce425fe83fcf1753afa1309bc0f48d468a74373
youngnam ju <youngnam@cnspartner.com>
2021-05-27 15:26:56 +0900

    BTOCSITE-552 앱 설치 팝업 디벨롭 - 안드로이드 구글플레이 앱 내 페이지 랜딩

--- src/js/app.common.js
+++ src/js/app.common.js
@@ -221,6 +221,28 @@ $(document).ready(function() {
             }
         }
 
+        /* goAppUrl : 앱실행및 해당 경로로 랜딩하는 함수
+        *  @path : 랜딩할 경로
+        */
+        goAppUrl = function(path) {
+            var scheme = 'lgeapp://goto?weblink='+(path ? path : location.pathname);
+
+            console.log("scheme %o",scheme);
+            
+            if( vcui.detect.isIOS ) {
+                var clickedAt = +new Date;
+                setTimeout( function () { 
+                    if (+new Date - clickedAt < 1000 ) { 
+                        // 앱스토어 이동 
+                        location.href = 'https://itunes.apple.com/app/id1561079401?mt=8'; 
+                    }
+                } ,500);
+                location.href = scheme; // 앱실행 
+            } else {
+                window.open(scheme+'#Intent;scheme=lgeapp;package=kr.co.lge.android;end;', '_blank');
+            }
+        }
+
         LGEAPPsetArBarcode();
         $(window).on({
             resize : function() {
