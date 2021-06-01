814a4dbc1e33c767cd8fe8bdb10ab379477ea6d8
윤권 <iamskies@cnspartner.com>
2021-06-01 15:15:42 +0900

    [UI] BTOCSITE-654 - [퍼블리싱] 스토어 > 배너 영역 스와이프 동작 개선 요청

--- src/js/home/storeHome.js
+++ src/js/home/storeHome.js
@@ -156,25 +156,12 @@ $(function(){
     }
 
     vcui.require(['ui/tab', 'ui/lazyLoaderSwitch', 'ui/carousel'], function () {
+        
+        
 
-        $('.ui_lifestyle_list').vcCarousel({
-            infinite: true,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            responsive: [{
-                breakpoint: 100000,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1,
-                }
-            },{
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }]                
-        });
+        $('.ui_lifestyle_list.idx1').vcCarousel({ infinite: true, slidesToShow: 4, slidesToScroll: 1, swipeToSlide: true, cssEase: 'ease-out', speed: 500, touchThreshold: 100, responsive: [{ breakpoint: 100000, settings: { slidesToShow: 4, slidesToScroll: 1, } },{ breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1 } }] });
+        $('.ui_lifestyle_list.idx2').vcCarousel({ infinite: true, slidesToShow: 4, slidesToScroll: 1, swipeToSlide: true, cssEase: 'cubic-bezier(0.33, 1, 0.68, 1)', speed : 350, touchThreshold: 100, responsive: [{ breakpoint: 100000, settings: { slidesToShow: 4, slidesToScroll: 1, } },{ breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1 } }] });
+        $('.ui_lifestyle_list.idx3').vcCarousel({ infinite: true, slidesToShow: 4, slidesToScroll: 1, responsive: [{ breakpoint: 100000, settings: { slidesToShow: 4, slidesToScroll: 1, } },{ breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1 } }] });
 
         $(window).on('breakpointchange.lifestyle', function(e){
 
