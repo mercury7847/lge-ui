9ee89a019832b91a093840351ecb7771a37c0174
kwr317 <kwr317@cnspartner.com>
2021-06-04 11:05:48 +0900

    앱실행시 AR 버튼 처음부터 보이게 수

--- src/js/home/main.js
+++ src/js/home/main.js
@@ -1123,6 +1123,13 @@ $(function () {
         // 플로팅 버튼 AR 관련 
         if (vcui.detect.isMobileDevice){
             var isApplication = isApp();
+
+            setTimeout(function(){
+                if (isApplication){
+                    $('.floating-menu.btn-app-ar').css('display', 'block');
+                }
+            }, 100);
+
             $(window).on('scroll.floating', function(){                
                 var scrollTop = $(window).scrollTop();
                 var hasTop = $('.floating-menu.top').hasClass('call-yet');
