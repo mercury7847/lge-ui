98352f8b44f6c9124e5aea807dd1fa360d3e4faf
kwr317 <kwr317@cnspartner.com>
2021-06-17 17:26:06 +0900

    앱 테스트 콘솔 추가

--- src/js/app.common.js
+++ src/js/app.common.js
@@ -11,8 +11,10 @@ MOBILE:     /mobile/.test(ua)
 */
 
 $(document).ready(function() {
+    console.log('앱 스크립트 시작');
     if (LGEAPPHostName != "cmsdev50.lge.co.kr" && LGEAPPHostName != "cms50.lge.co.kr") {
         if (isApp()) {
+            console.log('앱 확인');
             //헤더 앱 설정 버튼
             $('.mapExclusive').addClass('active');
             $('.mapExclusiveDss').hide();
@@ -30,6 +32,7 @@ $(document).ready(function() {
             });
 
             if($(".main-wrap").length > 0){
+                console.log('AR 버튼 추가');
                 //Quick메뉴 AR 버튼 추가
                 $("#floatBox .KRP0004").before('<div class="floating-menu cs-cst btn-app-ar"><div class="app-ar"><button href="javascript:void(0);"><span>AR</span><span class="app-ar-txt"><i></i>제품을 가상으로 배치해보세요</span></button></div></div>');
 
