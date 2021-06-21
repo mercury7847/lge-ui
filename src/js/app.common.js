3fa3087cfa0e104c535499af89bdfe5226516c49
이설근 <sg51.lee@cnspartner.com>
2021-06-21 15:59:36 +0900

    [UI] BTOCSITE-1828 모바일 앱 알림버튼 추가 - 알람아이콘,마이페이지 아이콘 위치 수정

--- src/js/app.common.js
+++ src/js/app.common.js
@@ -14,7 +14,6 @@ $(document).ready(function() {
     if (LGEAPPHostName != "cmsdev50.lge.co.kr" && LGEAPPHostName != "cms50.lge.co.kr") {
         if (isApp()) {
             //헤더 앱 설정 버튼
-            $('.mapExclusive').hide(); //210621
             $('.mapExclusive').addClass('active');
             $('.mapExclusiveDss').hide();
             $('.app-alarm-button').css('right','30px'); //210621
