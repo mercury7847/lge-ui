d17adc6a4eccb9cb2499fe02bdb2e3b77f917d59
songhee <songhee51.choi@cnspartner.com>
2021-06-25 16:13:37 +0900

    BTOCSITE-1376 고객센터 링크 오류건  js 수정

--- src/js/components/KRP0013.js
+++ src/js/components/KRP0013.js
@@ -10,29 +10,31 @@
             var self = this;
             //콤포넌트
             self.$component = $('section.component.KRP0013');
-            self.$packageButton = self.$component.find('.item-manual.package');
-            self.$selectPopup = $('#packageProductSelectPopup_01');
+            self.$packageButton = self.$component.find('.support-box .list-box a');
         },
         //팝업 버튼 이벤트
         bindPopupEvents: function() {
             var self = this;
             //BTOCSITE-1376 사용설명서 팝업 열기
             self.$packageButton.on('click', function(e){
-                e.preventDefault();
-                self.$selectPopup.vcModal({opener: this});
+                var $this = $(this);
+                if( $this.hasClass('package')) {
+                    e.preventDefault();
+                    var popupId = $this.attr('href');
+                    $(popupId).vcModal({opener: this});
+                }
             })
             
             //BTOCSITE-1376 사용설명서 팝업 푸터 닫기버튼
-            self.$selectPopup.find('.pop-footer .btn').on('click', function(e){
+            $('.popup-package-model').find('.pop-footer .btn').on('click', function(e){
                 var _self = this;
-                self.packageSelect(_self)
+                var $targetPopup = $(this).closest('.popup-package-model')
+                self.packageSelect(_self, $targetPopup)
             })
         },
-        packageSelect: function(alertTarget){
-            //BTOCSITE-1376 사용설명서 팝업 푸터 닫기버튼 클릭시 실행 이벤트
-            var self = this;
-            var $radio = self.$selectPopup.find('.model-list input:radio');
-
+        packageSelect: function(alertTarget, $targetPopup){
+            //BTOCSITE-1376 사용설명서 팝업 푸터 닫기버튼 클릭시 실행 이벤트            
+            var $radio = $targetPopup.find('.model-list input:radio');
             if( !$radio.filter(':checked').length ) {
                 var msgTxt = '제품을 선택해주세요';
                 lgkorUI.alert("", {title: msgTxt}, alertTarget);
