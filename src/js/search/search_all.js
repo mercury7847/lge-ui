8fbbdec7a468ed6d3d80e4503159ab1f294e73d4
kang youngjun <youngjun@dfluid.kr>
2021-07-08 10:19:49 +0900

    BTOCSITE-1101 [GA360] 검색 결과 내에서 소모품 관련 data-ec-product 값 부재

--- src/js/search/search_all.js
+++ src/js/search/search_all.js
@@ -94,14 +94,14 @@
                 '<div class="result-tit"><strong>{{#raw title}}</strong></div>' +
                 '<div class="result-detail">' +
                     '<div class="desc"><span>{{#raw desc}}</span></div>' +
-                    // '<div class="info-btm">' +
-                    //     '{{#if hash && hash.length>0}}' +
-                    //         '<div class="text hashtag-wrap">' +
-                    //             '{{#each item in hash}}<span class="hashtag"><span>#</span>{{item}}</span>{{/each}}' +
-                    //         '</div>' +
-                    //     '{{/if}}' +
-                    //     '<span class="text date"><span>{{date}}</span>' +
-                    // '</div>' +
+                    '<div class="info-btm">' +
+                        '{{#if hash && hash.length>0}}' +
+                            '<div class="text hashtag-wrap">' +
+                                '{{#each item in hash}}<span class="hashtag"><span>#</span>{{item}}</span>{{/each}}' +
+                            '</div>' +
+                        '{{/if}}' +
+                        '<span class="text date"><span>{{date}}</span>' +
+                    '</div>' +
                 '</div>' +
             '</div>' +
         '</div>' +
