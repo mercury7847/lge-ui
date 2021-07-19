7c6b3b0ee033640b51b7dbad7167a4d7bfa0bdb0
강영준 <kangyj12@cnspartner.com>
2021-07-20 00:32:03 +0900

    BTOCSITE-3128

--- src/js/search/search_all.js
+++ src/js/search/search_all.js
@@ -920,7 +920,7 @@
                         $list_ul.empty();
                         var $div = $("<div/>");
                         arr.forEach(function(item, index) {
-                            if(index > 1) return;
+                            if(index > 1) return; //BTOCSITE-3128 스토리 노출 건 4개에서 2개로 변경
                             if(!item.hash) {
                                 item.hash = [];
                             }
@@ -934,7 +934,7 @@
                         noData = false;
 
                         var $btnLink = $resultListWrap.find('div.btn-area a.btn-link:eq(0)');
-                        if($btnLink.length > 0 && count < 2) {
+                        if($btnLink.length > 0 && count < 2) { //BTOCSITE-3128 스토리 노출 건 4개에서 2개로 변경
                             $btnLink.hide();
                         } else {
                             $btnLink.show();
