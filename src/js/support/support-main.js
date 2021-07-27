ba92f7552c4c8912bde7aae3d09d211a214db6b4
youngnam ju <youngnam@cnspartner.com>
2021-07-27 18:42:04 +0900

    BTOCSITE-3577 고객지원 > Main 의 출장서비스 예약 영역에 제품 설치/철거 예약 탭 및 내용 추가

--- src/js/support/support-main.js
+++ src/js/support/support-main.js
@@ -586,14 +586,13 @@
                         }
                     });
 
-
                     // 20210727 BTOCSITE-3577 고객지원 > Main 의 출장서비스 예약 영역에 제품 설치/철거 예약 탭 및 내용 추가
                     self.el.container.find('.btn-installDemolition').on('click', function(){
                         var _self = this;
                         var href = $(this).data("href");
                         var target = $(this).data("target");
 
-                        lgkorUI.confirm( "제품 이전 설치/철거 서비스는 전문업체인 (주)엘액스판토스 홈페이지를 통해 예액이 가능합니다.",{
+                        lgkorUI.confirm( "제품 이전 설치/철거 서비스는 <b>전문업체인</b><br> <b>(주)엘엑스판토스 홈페이지를 통해 예약이<br> 가능합니다.</b><br><br><b>(주)엘엑스판토스로 이동 하시겠습니까?</b>",{
                             typeClass:'type2',
                             title:'',
                             okBtnName: '네',
@@ -608,10 +607,7 @@
                                 } 
                             }
                         });
-
                     });
-
-
 
                     self.el.container.find('.btn-address').on('click', function() { 
                         self.addressFinder.open(function(data) { 
