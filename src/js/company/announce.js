6bc2c54f8f1817a08cb357721b8e1e5c16e3893d
김태경 <tk@cnspartner.com>
2021-08-05 16:54:44 +0900

    [si-company] 전자공고 수정

--- src/js/company/announce.js
+++ src/js/company/announce.js
@@ -2,7 +2,7 @@
     var listItemTemplate =
 		 '<tr>'
 		+	'<td class="board-tit">'
-		+		'<a href="/company/investor/announceDetail?anncmNo={{announcementNo}}&page={{page}}">'
+		+		'<a href="/company/investor/announceView?anncmNo={{announcementNo}}&page={{page}}">'
 		+			'{{#if (isNew == "Y")}}'
 		+				'<p class="new">NEW</p>'
 		+			'{{/if}}'
