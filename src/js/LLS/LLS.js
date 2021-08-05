298fd67f82ea0b523f1103eef96ac556e43bcf99
kangyj12 <kangyj12@cnspartner.com>
2021-08-05 13:28:55 +0900

    3019

--- src/js/LLS/LLS.js
+++ src/js/LLS/LLS.js
@@ -1,11 +1,38 @@
 var lls = {
     init: function(){
         var self = this;
+
+        self.settings();
+        self.bindEvent();
         self.heroSlider();
         self.highlightSlider();
         self.onbroadProductSlider();
     },
-    bindEvent: function(){},
+    settings: function(){
+        var self = this;
+        self.$llsMain = $('.lls-main');
+        self.$eventSection = self.$llsMain.find('.event-announced');
+        self.$eventList = self.$eventSection.find('.event-item-list');
+        self.$eventAnchor = self.$eventList.find('.item-list-anchor');
+    },
+    bindEvent: function(){
+        var self = this;
+
+        self.$eventAnchor.on('click', function(e){
+            e.preventDefault();
+            self.requestModal(this);
+        });
+    },
+    requestModal: function(dm) {
+        var _self = this;
+        var ajaxUrl = $(dm).attr('href');
+        window.open(ajaxUrl,'','width=912,height=760,scrollbars=yes');
+        /*
+        lgkorUI.requestAjaxData(ajaxUrl, null, function(result){
+            _self.openModalFromHtml(result);
+        }, null, "html");
+        */
+    },
     heroSlider: function(){
         var heroSwiper = new Swiper('.hero-slider', {
             loop:true,
