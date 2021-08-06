e11c8773e11df6fd2b9374260d08bf325e668ecc
김현미 <ssal07@cnspartner.com>
2021-08-06 17:14:29 +0900

    ESG 데이터 테이블 간격 수정

--- src/js/company/company.js
+++ src/js/company/company.js
@@ -32,46 +32,31 @@ $(document).ready(function() {
 	$(".view_more").on('click', function(e){
 		var _this = $(this);
 		if(_this.hasClass("on")){
-			$(this).removeClass("on").html("ejqhrl");
+			$(this).removeClass("on").html("더보기");
 			$(this).parent().find(".content").removeClass("on")
 		}else{
 			$(this).addClass("on").html("닫기");
 			$(this).parent().find(".content").addClass("on")
 		}
     });
-	
   
 	setTimeout(function(){
 		// 탭 슬라이드 위치 이동
 		fcTabScrollLeft($('.border-type .tabs'));
 		fcTabScrollLeft($('.btn-type .tabs'));
 		// 첫번째 아코디언 열기
-		$(".accordion-wrap").each(function(index){
-			$(this).find("li.lists:first-child").find("a.accord-btn").trigger("click");
-		});
-		// 아코디언 클릭한 높이로 이동
-		$(".accordion-wrap li.lists a").on('click', function(e){
-			var scrollPosition = $(this).offset().top;
-			scrollPosition = scrollPosition - 100;
-			$('html, body').stop().animate({scrollTop: scrollPosition}, 500);
-		});
+		fcOpenAccordion();
 	}, 500);
-	
-	// 지난자료보기
-	$(".cont_view_more>.content").css("display", "none");
-	$(".cont_view_more>.btnArea>.btn").on('click', function(e) {
-		if ($(".cont_view_more>.content").css("display") == 'none') {
-			$(".cont_view_more>.content").css("display", "");
-			$(".cont_view_more>.btnArea span").attr("class", "close").text("지난 자료 닫기");
-			$(".cont_view_more>.btnArea>.btn").addClass("on");
-		} else {
-			$(".cont_view_more>.content").css("display", "none");
-			$(".cont_view_more>.btnArea span").attr("class", "open").text("지난 자료 열기");
-			$(".cont_view_more>.btnArea>.btn").removeClass("on");
-		}
-		e.preventDefault();
-	});
 });
+
+// 첫번째 아코디언 열기
+function fcOpenAccordion(){
+  if($(".tabs-contents .accordion-wrap").length > 0){
+    $(".tabs-contents .accordion-wrap li.lists:first-child").find("a.accord-btn").trigger("click");
+  }else{
+    $($(".accordion-wrap li.lists:first-child")[0]).find("a.accord-btn").trigger("click");
+  }
+}
 
 // 탭 슬라이드 위치 이동
 function fcTabScrollLeft(tab){
