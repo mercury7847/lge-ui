4cf98891999ce7a36a0e5d96bcb5a20eeeef07c4
김현미 <ssal07@cnspartner.com>
2021-08-09 22:42:12 +0900

    차트수정

--- src/js/company/company.js
+++ src/js/company/company.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     // 아코디언 닫기 버튼
 	$(".accordion-wrap .menu-close").on('click', function(e){
 		var heada = $(this).parent().parent().parent().find(".head a");
@@ -47,6 +46,28 @@ $(document).ready(function() {
 		// 첫번째 아코디언 열기
 		fcOpenAccordion();
 	}, 500);
+	
+	// 지난자료보기
+	$(".cont_view_more>.content").css("display", "none");
+	$(".cont_view_more>.btnArea>.btn").on('click', function(e) {
+		if ($(".cont_view_more>.content").css("display") == 'none') {
+			$(".cont_view_more>.content").css("display", "");
+			$(".cont_view_more>.btnArea span").attr("class", "close").text("지난 자료 닫기");
+			$(".cont_view_more>.btnArea>.btn").addClass("on");
+		} else {
+			$(".cont_view_more>.content").css("display", "none");
+			$(".cont_view_more>.btnArea span").attr("class", "open").text("지난 자료 열기");
+			$(".cont_view_more>.btnArea>.btn").removeClass("on");
+		}
+		e.preventDefault();
+	});
+	
+	//Google Tag Manager (noscript)
+	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-WL297VL');
 });
 
 // 첫번째 아코디언 열기
