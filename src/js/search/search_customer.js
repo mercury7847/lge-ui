652b1af9f902f0f5b2189fe697ce6b077a6a0ada
youngnam ju <youngnam@cnspartner.com>
2021-08-18 12:47:45 +0900

    BTOCSITE-4023 [GA360] 검색결과 내 소모품 관련 데이터레이어 변수 값 수정 요청

--- src/js/search/search_customer.js
+++ src/js/search/search_customer.js
@@ -220,9 +220,9 @@ if ('scrollRestoration' in history) {
                     "model_id":item.modelId,
                     "model_sku":item.salesModelCode + '.' + item.salesSuffixCode,
                     "model_gubun":(item.rentalTabFlag == "Y" && item.obsFlag == "N") ? "케어솔루션" : "일반제품",
-                    "rental_price":(item.carePrice && item.carePrice > 0) ? item.carePrice : "",
-                    "price":(item.originalPrice && item.originalPrice > 0) ? item.originalPrice : "",
-                    "discounted_price":(item.price && item.price > 0) ? item.price : "",
+                    "rental_price":(item.carePrice && item.carePrice > 0) ? ""+item.carePrice : "",
+                    "price":(item.originalPrice && item.originalPrice > 0) ? ""+item.originalPrice : "",
+                    "discounted_price":(item.price && item.price > 0) ? ""+item.price : "",
                     "brand":"LG",
                     "category":item.superCategoryName + "/" + item.categoryName
                 }
