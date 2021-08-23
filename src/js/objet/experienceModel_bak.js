1aeb83eb0a2e624c55333c55f96a1a7fcab41ffe
최주영 <hani@cnspartner.com>
2021-08-23 18:33:00 +0900

    [UI]BTOCSITE-4470 - 오브제컬렉션 체험 제품 비노출 요청

--- src/js/objet/experienceModel_bak.js
+++ src/js/objet/experienceModel_bak.js
@@ -738,10 +738,154 @@
                                     }
                                 }]
                             },       /* 매직 스페이스 제품 출시  연기로 주석처리하였음 출시되면 주석 풀면 됨 20210428 기준 end  (210719 주석해제) */
+                            {
+                            name: "베이직",
+                            defaultCode: "M871AAA041",
+                            leaderImg: "/lg5-common/images/OBJ/experience/leader/leader_img_M871AAA041.png",
+                            simulImg: "/lg5-common/images/OBJ/experience/leader/simul_img_M871AAA041.png",
+                            defaultPrice: "0",
+                            memberDiscount: "0",
+                            directDiscount: "0",
+                            magicSpace: 0,
+                            energy: 1,
+                            knockOn: false,
+                            door: {
+                                count: 4,
+                                door1: {
+                                    name: "상단(좌)",
+                                    code: "D870TT",
+                                    defaultPrice: "0",
+                                    memberDiscount: "0",
+                                    directDiscount: "0",
+                                },
+                                door2: {
+                                    name: "상단(우)",
+                                    code: "D870TT",
+                                    defaultPrice: "0",
+                                    memberDiscount: "0",
+                                    directDiscount: "0",
+                                },
+                                door3: {
+                                    name: "하단(좌)",
+                                    code: "D870BB",
+                                    defaultPrice: "0",
+                                    memberDiscount: "0",
+                                    directDiscount: "0",
+                                },
+                                door4: {
+                                    name: "하단(우)",
+                                    code: "D870BB",
+                                    defaultPrice: "0",
+                                    memberDiscount: "0",
+                                    directDiscount: "0",
+                                }
 
-                            /* 주영 베이지 삭제 */
+                            },
+                            doorColorData: [
+                                /* 210719 BTOCSITE-2346 냉장고 > 상냉장/하냉동 > 베이직 선택 시 '페닉스'재질 컬러칩 비노출 start
+                                {
+                                    doorMaterial: {
+                                        name: "페닉스",
+                                        enName: "FENIX",
+                                        code: "F",
+                                        desc: "감각적인 디자인을 완성하는 프리미엄 신소재로 손 끝은 스치는 부드러움과 스스로 재생되는 신비로움을 경험할 수 있습니다.",
+                                        descImg: "/lg5-common/images/OBJ/simulator/img/img-fenix.jpg",
+                                        doorColor: [{
+                                            name: "보타닉",
+                                            code: "BT",
+                                            mixingCode: "FBT"
 
-                              {
+                                        }, {
+                                            name: "샌드",
+                                            code: "SD",
+                                            mixingCode: "FSD"
+                                        }, {
+                                            name: "스톤",
+                                            code: "ST",
+                                            mixingCode: "FST"
+                                        }]
+                                    }
+
+                                },210719 BTOCSITE-2346 냉장고 > 상냉장/하냉동 > 베이직 선택 시 '페닉스'재질 컬러칩 비노출 end */
+                                {
+                                    doorMaterial: {
+                                        name: "솔리드",
+                                        enName: "Solid Metal",
+                                        code: "S",
+                                        desc: "메탈 본연의 세련됨과 트렌드를 담은 무광의 완벽한 조화. 매트한 스테인리스가 모던하고 아름다운 공간으로 연출합니다.",
+                                        descImg: "/lg5-common/images/OBJ/simulator/img/img-Solid.jpg",
+                                        doorColor: [{
+                                            name: "맨해튼 미드나잇",
+                                            code: "MT",
+                                            mixingCode: "SMT"
+                                        }, {
+                                            name: "실버",
+                                            code: "SV",
+                                            mixingCode: "SSV"
+                                        }, {
+                                            name: "그린",
+                                            code: "GR",
+                                            mixingCode: "SGR"
+                                        }]
+                                    }
+                                }, {
+                                    doorMaterial: {
+                                        name: "미스트",
+                                        enName: "Mist Glass",
+                                        code: "G",
+                                        desc: "고운 안개처럼 부드러운 터치감을 느낄 수 있는 매트한 유리 소재로, 공간에 은은하게 어우러집니다.",
+                                        descImg: "/lg5-common/images/OBJ/simulator/img/img-Mist.jpg",
+                                        doorColor: [{
+                                            name: "베이지",
+                                            code: "BE",
+                                            mixingCode: "GBE"
+                                        }, {
+                                            name: "핑크",
+                                            code: "PK",
+                                            mixingCode: "GPK"
+                                        }, {
+                                            name: "민트",
+                                            code: "MN",
+                                            mixingCode: "GMN"
+                                        }, {
+                                            name: "실버",
+                                            code: "SV",
+                                            mixingCode: "GSV"
+                                        }, {
+                                            name: "레드우드",
+                                            code: "RD",
+                                            mixingCode: "GRD"
+                                        }, {
+                                            name: "클레이 브라운",
+                                            code: "CL",
+                                            mixingCode: "GCL"
+                                        }]
+                                        //210719 BTOCSITE-2346 신규 컬러 추가 - 레드우드,클레이브라운
+                                    }
+                                }, {
+                                    doorMaterial: {
+                                        name: "네이쳐",
+                                        enName: "Nature Metal",
+                                        code: "M",
+                                        desc: "자연을 본뜬 질감 메탈 소재로 어느 공간에 두어도 차분하고 편안한 감각을 표현해주는 소재입니다.",
+                                        descImg: "/lg5-common/images/OBJ/simulator/img/img-Nature.jpg",
+                                        doorColor: [{
+                                            name: "그레이",
+                                            code: "GY",
+                                            mixingCode: "MGY"
+                                        }, {
+                                            name: "블랙",
+                                            code: "BK",
+                                            mixingCode: "MBK"
+                                        }, {
+                                            name: "화이트",
+                                            code: "WH",
+                                            mixingCode: "MWH"
+                                        }]
+                                    }
+                                }]
+                            },
+                            {
                                 name: "빌트인 타입",
                                 defaultCode: "M620AAA351",
                                 leaderImg: "/lg5-common/images/OBJ/experience/leader/leader_img_M620AAA351.png",
