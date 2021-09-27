86bf2b439bb78b7240fb3df493463940913a31ff
geumhye.oh <ghoh@cnspartner.com>
2021-09-27 13:09:53 +0900

    BTOCSITE-4785 매장 진열모델 판매 상시기획전 제작 의뢰

--- src/js/event/evtExhibitions.js
+++ src/js/event/evtExhibitions.js
@@ -35,7 +35,7 @@ function goPdpUrl() {
     $('.btn-pdp-alert').on('click', function(e) {
         e.preventDefault();
         var href = $(this).attr('href');
-        lgkorUI.alert('이동하실 제품 페이지의 가격 및 혜택은 LGE.COM 구매 가격입니다.<br>진열 제품은 ‘진열제품 구매가능매장 확인하기’를 통해 매장에서 저렴하게 구매하실 수 있습니다.', {
+        lgkorUI.alert('이동하실 제품 페이지의 가격 및 혜택은<br>LGE.COM 구매 가격입니다.<br>진열 제품은<br>‘진열제품 구매가능매장 확인하기’를 통해<br>매장에서 저렴하게 구매하실 수 있습니다.', {
             title: "",
             okBtnName: "확인",
             ok: function(){
