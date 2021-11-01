9533f270365319df9767869ecee5dd7fccafae2d
kangyj12 <kangyj12@cnspartner.com>
2021-11-01 11:36:07 +0900

    77

--- src/js/home/brandHome.js
+++ src/js/home/brandHome.js
@@ -417,7 +417,7 @@
             var self = this;
             var ajaxUrl = self.$searchPopup.data('ajaxUrl');
             var listTemplate =  '<li>' + 
-            '   <div class="icon-wrap"><i class="icon icon-{{imgName}}"><span class="blind">{{categoryName}} 아이콘</span></i></div>' + 
+            '   <div class="icon-wrap"><i class="icon icon-{{imgname}}"><span class="blind">{{categoryName}} 아이콘</span></i></div>' + 
             '   <div class="text">' + 
             '       <span class="name">{{categoryName}}</span>' + 
             '       <span class="serial-num">{{salesModelCode}}</span>' + 
