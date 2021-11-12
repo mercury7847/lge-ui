65de94f6c56582186d38f54d05166666f6fa0222
sooa.jeong <sooa51.jeong@cnspartner.com>
2021-11-12 17:32:28 +0900

    BTOCSITE-7448 렌탈제품 PDP 하단 유사 제품 추천로직 변경요청

--- src/js/components/KRP0011.js
+++ src/js/components/KRP0011.js
@@ -1,66 +1,59 @@
 (function() {
     var productItem =
-    '<li>' +
-        '<div class="item rec-items">'+
-            '<div class="product-image" aria-hidden="true">'+
-                '{{#if (bizType == "PRODUCT")}}'+
-                '<a href="#">'+
-                    '<img src="{{mediumImageAddr}}" alt="{{modelDisplayName}}">'+
-                '</a>'+
-                '{{/if}}' +
-                '{{#if (bizType == "CARESOLUTION")}}'+
-                '<a href="{{modelUrlPath}}">'+
-                    '<img src="{{mediumImageAddr}}" alt="{{modelDisplayName}}">'+
-                '</a>'+
-                '{{/if}}' +
-            '</div>'+
-            '<div class="product-contents">'+
-                '<div class="product-info">'+
-                    '<div class="product-name">'+
-                        '<a href="#">{{modelDisplayName}}</a>'+
-                    '</div>'+
-                    '<div class="sku">{{modelName}}</div>'+
-                    '{{#if (reviewsCount != "0")}}' +
-                    '<div class="review-info">'+
-                        '<a href="#">'+
+        '<li>' +
+            '<div class="item rec-items">'+
+                '<div class="product-image" aria-hidden="true">'+
+                    '<a href="{{modelUrlPath}}">'+
+                        '<img src="{{mediumImageAddr}}" alt="{{modelDisplayName}}">'+
+                    '</a>'+
+                '</div>'+
+                '<div class="product-contents">'+
+                    '<div class="product-info">'+
+                        '<div class="product-name">'+
+                            '<a href="#">{{modelDisplayName}}</a>'+
+                        '</div>'+
+                        '<div class="sku">{{modelName}}</div>'+
+                        '{{#if (reviewsCount != "0")}}' +
+                        '<div class="review-info">'+
                             '<div class="star is-review"><span class="blind">리뷰있음</span></div>'+
                             '<div class="average-rating"><span class="blind">평점</span>{{reviewsScore}}</div>'+
                             '<div class="review-count"><span class="blind">리뷰 수</span>({{reviewsCount}})</div>'+
-                        '</a>'+
+                        '</div>'+
+                        '{{/if}}' +
                     '</div>'+
-                    '{{/if}}' +
                 '</div>'+
-            '</div>'+
-            '{{#if (bizType == "PRODUCT")}}'+
-            '<div class="price-area">'+
-                '{{#if (obsOriginalPrice != "0")}}'+
-                '<div class="original">'+
-                    '<em class="blind">할인 전 정가</em>'+
-                    '<span class="price">{{obsOriginalPrice}}<em>원</em></span>'+
-                '</div>'+
-                '{{/if}}' +
-                '{{#if (obsSellingPrice != "0")}}'+
-                '<div class="total">'+
-                    '<em class="blind">할인 후 판매가</em>'+
-                    '<span class="price">{{obsSellingPrice}}<em>원</em></span>'+
-                '</div>'+
-                '{{/if}}' +
-            '</div>'+
-            '{{/if}}' +
-            '{{#if (bizType == "CARESOLUTION")}}'+
-            '<div class="product-bottom rental-type">'+
-                '<div class="price-area care">'+
-                    '<div class="total-price"><em class="text">기본 월 요금</em>'+
-                        '<span class="price"><em>월</em> 29,900<em>원</em></span>'+
+
+                '{{#if (tabName == "purchaseTab")}}'+
+                '<div class="price-area">'+
+                    '{{#if (obsSellingPrice != "0")}}'+
+                    '<div class="original">'+
+                        '<em class="blind">할인 전 정가</em>'+
+                        '<span class="price">{{vcui.number.addComma(obsOriginalPrice)}}<em>원</em></span>'+
                     '</div>'+
-                    '{{#if (visitPer != "null")}}'+
-                    '<span class="small-text">({{visitPer}}개월/1회 방문)</span>'+
-                    '{{/if}}' +
+                    '{{/if}}'+
+                    '{{#if (obsSellingPrice != "0")}}'+
+                    '<div class="total">'+
+                        '<em class="blind">할인 후 판매가</em>'+
+                        '<span class="price">{{vcui.number.addComma(obsSellingPrice)}}<em>원</em></span>'+
+                    '</div>'+
+                    '{{/if}}'+
                 '</div>'+
+                '{{/if}}'+
+
+                '{{#if (tabName == "rentalTab") && (years1TotAmt !=0)}}'+
+                '<div class="product-bottom rental-type">'+
+                    '<div class="price-area care">'+
+                        '<div class="total-price"><em class="text">기본 월 요금</em>'+
+                            '<span class="price"><em>월</em> {{vcui.number.addComma(years1TotAmt)}}<em>원</em></span>'+
+                        '</div>'+
+                        '{{#if (visitPer != "null")}}'+
+                            '<span class="small-text">({{visitPer}}개월/1회 방문)</span>'+
+                        '{{/if}}'+
+                    '</div>'+
+                '</div>'+
+                '{{/if}}'+
             '</div>'+
-            '{{/if}}' +
-        '</div>'+
-    '</li>';
+        '</li>';
 
     $(window).ready(function(){
 
@@ -83,27 +76,26 @@
 
                 self.$tabs = $('.option-tabs .tabs');
                 self.$tabList = self.$tabs.find('li a');
-                self.$recommendProductData = recommendProduct.data;
 
+                //스펙 비교하기 버튼
+                self.$compareModelIds = self.$section.find("bottom-area").data('modelIds');
+                self.$compareModelIdsRental = "";
 
             },
             bindEvents: function() {
                 var self = this;
 
-                //구매/렌탈 탭 클릭시 유사제품 상품 리스트 변경
+                //구매/렌탈 탭 클릭시 유사제품 상품, 각각의 금액으로 변경
                 self.$tabList.on('click',function(){
                     var $idx = $(this).parent().index();
                     self.$prodViewNow.empty();
-
+                    self.$prodRecommend.empty();
                     if($idx === 0){
-                    // 구매 탭
-                        console.log("구매 탭 클릭");
-                        self.makeProdList(recommendProduct.data[0],self.$prodRecommend);
+                        // 구매 탭
+                        self.makeProdList("purchaseTab", dataList.compareList);
                     }else if($idx === 1) {
-                    //렌탈 탭
-                        console.log("렌탈 탭 클릭");
-                        self.makeProdList(recommendProduct.data[1],self.$prodViewNow);
-                        console.log("makeProdList 실행");
+                        //렌탈 탭
+                        self.makeProdList("rentalTab", dataList.rentalCompareList);
                     }
                 });
 
@@ -115,20 +107,19 @@
                     lgkorUI.addEqualCompare($(this).data('modelIds'), url);
                 });
             },
-            makeProdList: function(data,target){
+            makeProdList: function(tabType, loopData){
                 var self = this;
+                var $dataProdInfo = dataList.productInfo;
 
+                $dataProdInfo.tabName = tabType;
+                $.each(data,function(idx,item){
+                    item.tabName = tabType;
+                });
 
-                if(typeof recommendProduct != 'undefined') {
-                    $.each(data, function(idx,item){
-                        target.append(vcui.template(productItem,item));
-                        if(idx === "compareList"){
-                            $.each(item.compareList,function(idx,item){
-                                target.append(vcui.template(productItem,item));
-                            })
-                        }
-                    })
-                }
+                self.$prodViewNow.append(vcui.template(productItem,$dataProdInfo));
+                $.each(data,function(idx,item){
+                    self.$prodRecommend.append(vcui.template(productItem,item))
+                });
             }
         }
         KRP0011.init();
