8a5b7cf8d0bb6cbee403551d5df5f15432fbc247
geumhye.oh <ghoh@cnspartner.com>
2021-11-18 14:20:06 +0900

    BTOCSITE-7314 PDP > 프로모션 배너 구좌 추가 요청 (+CMS Admin)

---
+++ src/js/components/KRP0042.js
@@ -0,0 +1,43 @@
+$(window).ready(function(){
+	if(!document.querySelector('.KRP0042')) return false;
+
+	$('.KRP0042').buildCommonUI();
+
+	vcui.require(['ui/carousel'], function () {
+
+		$('.KRP0042').find(".ui_carousel_slider").each(function(cdx, slide){
+			$(slide).on('carouselinit', function(e,data){
+
+				vcui.require(['ui/videoBox','ui/youtubeBox'], function(){
+
+					$('.KRP0042').find(".ui_carousel_slider .youtube-box").vcYoutubeBox();
+					$('.KRP0042').find(".ui_carousel_slider .animation-box").vcVideoBox();
+				});
+
+			}).vcCarousel({
+				infinite: false,
+				autoplay: true,
+				prevArrow:'.btn-arrow.prev',
+				nextArrow:'.btn-arrow.next',
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				playSelector: '.btn-play.play',
+				adaptiveHeight:true,
+				cssEase: 'cubic-bezier(0.33, 1, 0.68, 1)',
+				speed: 150,
+				touchThreshold: 100
+
+			}).on('carouselbeforechange', function(e, slide, prev, next){
+
+				if($(slide.$slides.get(prev)).attr("ui-modules") == "VideoBox"){
+					$(slide.$slides.get(prev)).find("video").get(0).pause();
+				}
+			}).on('carouselafterchange', function(e, slide, prev, next){
+
+				if($(slide.$slides.get(prev)).attr("ui-modules") == "VideoBox"){
+					$(slide.$slides.get(prev)).find("video").get(0).play();
+				}
+			});
+		});
+	});
+})
