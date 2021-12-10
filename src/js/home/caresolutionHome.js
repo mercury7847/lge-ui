1bc653d26ce583fbd64b7ff9b4ef598c879ce7c4
nabyeul <nabyeul@cnspartner.com>
2021-12-10 15:43:22 +0900

    BTOCSITE-6883 신규 WSG 적용 - 렌탈/케어

--- src/js/home/caresolutionHome.js
+++ src/js/home/caresolutionHome.js
@@ -334,5 +334,38 @@ $(function(){
             var url = $target.data('url');
             if(url) location.href = url;
         });
+
+        /* BTOCSITE-6883 신규 WSG 적용 - 렌탈/케어 */
+        // 히어로배너
+        $context.find('.ui_wide_slider').vcCarousel({
+            infinite: true,
+            autoplay: true,
+            slidesToScroll: 1,
+            slidesToShow: 1,
+            speed: 150,
+            touchThreshold: 100
+        }).on('carouselbeforechange', function(e, slide, prev, next){
+            heroBanner();
+        }).on('carouselafterchange', function(e, slide, prev, next){
+            heroBanner();
+        });
+
+        function heroBanner() {
+            var heroList = $('.hero-banner .slide-track > li');
+            var heroListAct = $('.hero-banner .slide-track > li.ui_carousel_current').index();
+            var heroListLens = heroList.length;
+            var custom = $('.custom-indi-wrap');
+            var slideCurrent = custom.find('.slide-page .current');
+            var slideCount = custom.find('.slide-page .count');        
+
+            if( heroListLens > 1) {
+                custom.show();
+                slideCurrent.text(heroListAct);
+                slideCount.text(heroListLens - 2);
+            }
+        }
+
+        heroBanner();
+        /* //BTOCSITE-6883 신규 WSG 적용 - 렌탈/케어 */
     }); 
 });
