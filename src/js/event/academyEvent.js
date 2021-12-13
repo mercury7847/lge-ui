b7bab1103c1122864b3000641ae37a670088f2e1
geumhye.oh <ghoh@cnspartner.com>
2021-12-13 13:48:30 +0900

    테스트중

--- src/js/event/academyEvent.js
+++ src/js/event/academyEvent.js
@@ -1,43 +1,71 @@
 (function() {
-        var emailCertified = {
-            init: function() {
-                var self = this;
-                    self.$contents = $('#academyPopup02');
-                    self.$submitForm = self.$contents.find('#emailCertifiedForm');
+    var validation;
 
-                    //self.url = self.$submitForm.data('ajax');
-                    $('#btnCertified').on('click', function() {
-                        alert(1);
-                        vcui.require(['ui/validation'], function () {
-                            self.validation = new vcui.ui.CsValidation('#emailCertifiedForm', { 
-                                register: {
-                                    email: {
-                                        required: true,
-                                        errorMsg: "이메일 주소를 다시 확인해주세요.",
-                                        msgTarget: '.err-block',
-                                        pattern : /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                                    }
+    var emailCertified = {
+        init: function() {
+            var self = this;
+            //self.param = {};
+
+            self.$cont = $('#academyPopup02');
+            self.$submitForm = self.$cont.find('#emailCertifiedForm');
+            self.$completeBtn = $('#btnCertified');
+
+            vcui.require(['ui/validation'], function () {
+                var register = {
+                    //이메일
+                    userEmail: {
+                        required: true,
+                        pattern : /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                        minLength: 1,
+                        maxLength: 50,
+                        msgTarget: '.err-block',
+                        errorMsg: '이메일 주소를 입력해주세요.',
+                        patternMsg: '@ac.kr 계정의 이메일을 입력해 주세요.',
+                        validate : function(value){
+                            var _pattern = new RegExp(this.pattern);
+                            if( _pattern.test(value) == true) {
+                                if( value.split('@')[0].length <= 30 && value.split('@')[1].length <= 20) {
+                                    console.log( value.split('@')[1]);
+                                    return true;
+                                } else {
+                                    return false;
                                 }
-                            });
-                        });
-                });
-            },
-            bindEvent: function() {
-                $('#academyPopup01 .btn-list').on('click', function(e) {
-                    e.preventDefault();
-                    $('#academyPopup01').vcModal('hide'); 
-                    location.href = "/benefits/exhibitions";
-                });
-                $('#academyPopup02 .btn-close').on('click', function(e) {
-                    e.preventDefault();
-                    $('#academyPopup02').vcModal('hide'); 
-                    location.reload();
-                });
-            }
-        };
+                            } else {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                validation = new vcui.ui.CsValidation('#emailCertifiedForm', {register:register});
+                self.bindEvent();
+            });
+
+        },
+        bindEvent: function() {
+            var self = this;
+            
+            //신청 버튼 클릭시
+            self.$completeBtn.on('click', function() {
+                var result = validation.validate();
+                //console.log(result);
+                //self.param = validation.getAllValues();
+
+                // if (result.success === true) {
+                //     lgkorUI.showLoading();
+                //     $('html, body').animate({
+                //         scrollTop: 0
+                //     }, 500);
+                //     lgkorUI.hideLoading();
+                // }else{
+                //     lgkorUI.alert('', {
+                //         title:'필수 입력정보가<br>입력되지 않았습니다.'
+                //     });
+                // }
+            });
+        },
+    }
+
+    $(window).ready(function() {
+        emailCertified.init();
+    });
 })();
-
-
-$(document).ready(function() {
-  // emailCertified.init();
-});
