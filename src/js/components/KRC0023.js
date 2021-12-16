9147945e2c0778ad8620e3c0051ed8bccb133de5
sooa.jeong <sooa51.jeong@cnspartner.com>
2021-12-16 15:06:27 +0900

    BTOCSITE-8625 LGE.COM PDP 내 캐로셀 컴포넌트 수정요청

--- src/js/components/KRC0023.js
+++ src/js/components/KRC0023.js
@@ -5,22 +5,42 @@ $(window).ready(function(){
 
     vcui.require(['ui/toggleCarousel'], function () {
 
-        $('.KRC0023').find('.ui_carousel_slider2').vcToggleCarousel({
-            pcOption: "unbuild",
-            cssEase: 'cubic-bezier(0.33, 1, 0.68, 1)',
-            touchThreshold: 100,
-            speed: 150,
-            mobileOption: {
-                infinite: true,
-                dots: true,
-                slidesToShow: 1, 
-                slidesToScroll: 1,
-                cssEase: 'cubic-bezier(0.33, 1, 0.68, 1)',
-                speed: 150,
-                touchThreshold: 100
+        $('.KRC0023').each(function(){
+            //BTOCSITE-9570 진열기획전 페이지 수정 요청으로 slidesToShowMore 케이스 추가
+            if($(this).hasClass('slidesToShowMore')){
+                $(this).find('.ui_carousel_slider2').vcToggleCarousel({
+                    pcOption: "unbuild",
+                    cssEase: 'cubic-bezier(0.33, 1, 0.68, 1)',
+                    touchThreshold: 100,
+                    speed: 150,
+                    mobileOption: {
+                        infinite: false,
+                        dots: true,
+                        slidesToShow: 1.28,
+                        slidesToScroll: 1,
+                        cssEase: 'cubic-bezier(0.33, 1, 0.68, 1)',
+                        speed: 150,
+                        touchThreshold: 100
+                    }
+                });
+            }else{
+                $(this).find('.ui_carousel_slider2').vcToggleCarousel({
+                    pcOption: "unbuild",
+                    cssEase: 'cubic-bezier(0.33, 1, 0.68, 1)',
+                    touchThreshold: 100,
+                    speed: 150,
+                    mobileOption: {
+                        infinite: true,
+                        dots: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        cssEase: 'cubic-bezier(0.33, 1, 0.68, 1)',
+                        speed: 150,
+                        touchThreshold: 100
+                    }
+                });
             }
-        });
-
+        })
     });    
 
 })
