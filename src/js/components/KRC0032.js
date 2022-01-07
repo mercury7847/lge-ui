baaba7f17e9fb260b15276322438054106dbbf29
LGE\smlee232.lee <smlee232@cnspartner.com>
2022-01-07 16:58:53 +0900

    BTOCSITE-8039 WCMS 컴포넌트 개선 요청 건 : BTOCSITE-9207 충돌 싱크 맞추기 (32,35)

--- src/js/components/KRC0032.js
+++ src/js/components/KRC0032.js
@@ -160,23 +160,30 @@ $(window).ready(function(){
 		var infoDisclaimer = $('.KRC0032').find('.carousel-box');
 
 		infoDisclaimer.each(function(cdx, item){
+			//console.log("11111", $(item).find('.title').attr('id'));
 			var titleAttr = $(item).attr("id");
 			var slideID = titleAttr;
 
 			$(item).find('.drop-info .dropInfo_openBtn').on('click', function(e){
-				e.preventDefault();
 				var mybtnAttr = $(this).attr('aria-describedby', slideID);
 
+				//console.log("콘텐츠의 아이디", titleAttr);
+				//console.log("내가 찍힌", mybtnAttr.attr('aria-describedby'));
+
 				if(titleAttr = mybtnAttr) {
+				    //console.log("111");
 				    $(item).find('.dropContent').addClass('on');
+				    //$(this).hide();
 				}
 			});
 
 			$(item).find('.drop-info .dropInfo_closeBtn').on('click', function(e){
 			    var mybtnAttr = $(this).attr('aria-describedby', slideID);
-				e.preventDefault();
+	
 			    if(titleAttr = mybtnAttr) {
+			        //console.log("111");
 			        $(item).find('.dropContent').removeClass('on');
+			        //$(item).find('.drop-info .dropInfo_openBtn').show();
 			    }
 			});
 		});
