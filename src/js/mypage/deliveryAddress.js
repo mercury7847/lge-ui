8a9f312111cc14d7e5161a8ef93f4083b656b1bf
LGE\smlee232.lee <smlee232@cnspartner.com>
2022-01-11 09:25:43 +0900

    BTOCSITE-5938-478 - [PC]마이페이지 > 배송지 주소 관리 연락처 영역 스크롤로  ±1되는 현상 - DEV

--- src/js/mypage/deliveryAddress.js
+++ src/js/mypage/deliveryAddress.js
@@ -153,6 +153,8 @@
 
         $('input[type=number]').on('keydown', function(e){
             return e.keyCode !== 69;
+        }).on('mousewheel', function(e){ // BTOCSITE-5938-478
+            if($(this).attr('name')=='phoneNumber'||$(this).attr('name')=='telephoneNumber') return false;
         });
     }
 
