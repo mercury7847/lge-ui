48286fb5dfdb87ee5f1422c8164d39544fa1ec93
sg51.lee <sg51.lee@cnspartner.com>
2022-01-14 11:14:01 +0900

    [UI] BTOCSITE-9066 신규 WSG 적용 - 고객지원 - dev

--- src/js/support/support-main.js
+++ src/js/support/support-main.js
@@ -308,8 +308,8 @@
                             {
                             breakpoint: 1024,
                             settings: {
-                                slidesToShow: 3,
-                                slidesToScroll:3
+                                slidesToShow: 2.16,
+                                slidesToScroll:2
                                 }
                             },
                             {
