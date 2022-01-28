7497ce58c7217b704ab8c0c84298bb13714bde04
youngnam ju <youngnam@cnspartner.com>
2022-01-28 11:38:59 +0900

    BTOCSITE-11602 [긴급] 2022년 설연휴 팝업 공지 요청

--- src/js/support/support-main.js
+++ src/js/support/support-main.js
@@ -1050,7 +1050,7 @@
                         // }
                         $popup.filter('.active').stop().fadeIn();
 
-                        $popup.filter('.active').first().focus();
+                        // $popup.filter('.active').first().focus();
 
                         if( !vcui.detect.isMobileDevice) {
                             $popup.filter('.active').not('.mCustomScrollbar').find('.pop-conts').mCustomScrollbar();
