0c6f0058d0e615f2d48693d355616a0186797a2a
LGE\smlee232.lee <smlee232@cnspartner.com>
2022-02-03 16:41:26 +0900

    BTOCSITE-9207 법무문구 디스클라이머 삽입을 위한 컴포넌트 요청 - dev : KRC0035 디스클레이머 오픈시 영역

--- src/js/components/KRC0035.js
+++ src/js/components/KRC0035.js
@@ -33,6 +33,22 @@ $(document).ready(function(){
 	var visualWrapBtn = $('.visual-wrap').find('.dropInfo_openBtn'); //모바일
 	var textWrapBtn = $('.text-wrap').find('.dropInfo_openBtn'); //피씨
 	var hdlineTop = $('.m-hdline-top');
+	// s : BTOCSITE-9207 2022-02-04
+	var toggleInfoDisclaimer = function(_this, isOpen) {
+		var target = _this.parents('.KRC0035');
+		var height = Math.max.apply(null, target.find('.dropContent.on').map(function() {
+			return $(this).outerHeight();
+		})) - _this.outerHeight();
+		if(isOpen) {
+			if (_this.offset().top +_this.outerHeight() < target.offset().top + target.height()) {
+				height = height - ((target.offset().top + target.height()) - (_this.offset().top +_this.outerHeight()));
+			}
+			if (height > 0) target.css('marginBottom', height);
+		}else {
+			target.removeAttr('style');
+		}
+	}
+	// e : BTOCSITE-9207 2022-02-04
 
 	
 
@@ -46,18 +62,21 @@ $(document).ready(function(){
 				//console.log("모바일에만 작동");
 				$(item).find('.visual-wrap .dropContent').addClass('on');
 				//$(this).hide(); //버튼이 사라지면서 컨텐츠 높낮이의 변화가 생겨 삭제
+				toggleInfoDisclaimer($(this), true) // BTOCSITE-9207 2022-02-04
 			});
 
 			$(item).find(textWrapBtn).on('click', function(e){
 				//console.log("피씨에만 작동");
 				$(item).find('.text-wrap .dropContent').addClass('on');
 				//$(this).hide(); //버튼이 사라지면서 컨텐츠 높낮이의 변화가 생겨 삭제
+				toggleInfoDisclaimer($(this), true) // BTOCSITE-9207 2022-02-04
 			});
 		} else {
 			$(item).find('.drop-info .dropInfo_openBtn').on('click', function(e){
 				//console.log("공통");
 				$(item).find('.dropContent').addClass('on');
 				//$(this).hide(); //버튼이 사라지면서 컨텐츠 높낮이의 변화가 생겨 삭제
+				toggleInfoDisclaimer($(this), true) // BTOCSITE-9207 2022-02-04
 			});
 		}
 	
@@ -65,6 +84,7 @@ $(document).ready(function(){
         $(item).find('.drop-info .dropInfo_closeBtn').on('click', function(e){
 			$(item).find('.dropContent').removeClass('on');
 			$(item).find('.drop-info .dropInfo_openBtn').show();
+			toggleInfoDisclaimer($(this), false) // BTOCSITE-9207 2022-02-04
         });
     });
 	/* //BTOCSITE-9207 : 디스클라이머 컴포넌트 추가 */
