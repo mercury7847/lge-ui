d6f1a57153d61cf3196a9684db8a4423777c7351
youngnam ju <youngnam@cnspartner.com>
2022-02-05 15:52:44 +0900

    BTOCSITE-12128 홈 영역 UI 성능 개선 작업

--- src/js/components/KRP0005.js
+++ src/js/components/KRP0005.js
@@ -237,10 +237,10 @@
         if (isSwipe && $('#floatBox').find('.floating-wrap').length < 1){
             var domInsertCheck = false;
             $('#sw_con .swiper-slide').one('DOMNodeInserted', function(e) {
-                console.log("dom insert %o",e.target);
+                // console.log("dom insert %o",e.target);
 
                 if(!domInsertCheck) {
-                    console.log('krp0005 init');
+                    // console.log('krp0005 init');
                     var floatingWrap = $('.floating-wrap').remove();
                     var btnFloatingWrap = $('.btn-floating-wrap').remove();
                     $('#floatBox').append(btnFloatingWrap);
