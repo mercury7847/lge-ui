cd547d87294b0d43abe86e37188536f68cbea0d3
yeorang.jo <yeorang.jo@cnspartner.com>
2022-02-09 17:55:52 +0900

    BTOCSITE-11191 진열기획전 페이지 지점별 필터 추가 건 퍼블리싱 진행

--- src/js/event/evtExhibitions.js
+++ src/js/event/evtExhibitions.js
@@ -49,3 +49,142 @@ function goPdpUrl() {
     });
 }
 // BTOCSITE-4785 e
+
+// BTOCSITE-11191 지점별 필터 기능 추가
+var evFilterItemTemplate =
+'<li data-uniq-id="{{uniqId}}">' +
+    '<div class="item evt-item">' +
+        '{{#if promotionBadges}}'+
+        '<div class="badge">' +
+            '<div class="flag-wrap image-type left">'+
+                '{{#each badge in promotionBadges}}'+
+                    '{{#if badge.badgeImgUrl}}'+
+                    '<span class="big-flag">'+
+                        '<img src="{{badge.badgeImgUrl}}" alt="{{badge.badgeName}}">'+
+                    '</span>'+
+                    '{{/if}}'+
+                '{{/each}}'+
+            '</div>'+
+        '</div>'+
+        '{{/if}}' +
+        '<div class="product-image" aria-hidden="true">' +
+            '<a href="{{modelUrlPath}}" class="btn-pdp-alert"><img src="{{image}}" alt="{{categoryName}} {{modelDisplayAltName}} {{sku}} 썸네일"></a>' +
+        '</div>' +
+        '<div class="product-contents">' +
+            '<div class="product-info">' +
+                '<div class="product-name">' +
+                    '<a href="{{modelUrlPath}}" class="btn-pdp-alert">{{#raw modelDisplayName}}</a>' +
+                '</div>' +
+                '<div class="sku">{{#if modelName}}{{modelName}}{{/if}}</div>' +
+                '<div class="review-info">' +
+                    '<a href="#">' +
+                        '{{#if (reviewsCount != "0")}}' +
+                        '<div class="star is-review"><span class="blind">리뷰있음</span></div>' +
+                        '<div class="average-rating"><span class="blind">평점</span>{{reviewsScore}}</div>' +
+                        '<div class="review-count"><span class="blind">리뷰 수</span>({{reviewsCount}})</div>' +
+                        '{{/if}}' +
+                    '</a>' +
+                '</div>' +
+                '<ul class="spec-info">' +
+                    '{{#if firstBulletName}}<li>{{firstBulletName}}</li>{{/if}}'+
+                    '{{#if showBulletFeatures}}' +
+                    '{{#each item in showBulletFeatures}}' +
+                    '<li>{{#raw item.specText}}</li>' +
+                    '{{/each}}' +
+                    '{{/if}}' +
+                    '{{#if lastBulletName}}<li>{{lastBulletName}}' +
+                    '{{#if (subCategoryId == "CT50000070")}}<span class="care-n">,</span><span class="redcare-option">1년 무상케어</span>{{/if}}' +
+                    '</li>{{/if}}'+
+                '</ul>' +
+            '</div>' +
+        '</div>'+
+        '<div class="product-bottom">' +
+            '{{#if checkPriceFlag}}'+
+
+                //케어솔루션 경우
+                '{{#if bizType == "CARESOLUTION"}}' +
+                '<div class="price-area care">' +
+                    '<div class="total-price">' +
+                        '<em class="text">기본 월 요금</em>' +
+                        '<span class="price"><em>월</em> {{years1TotAmt}}<em>원</em></span>' +
+                    '</div>' +
+
+                    '{{#if visitPer != "0"}}' +
+                     '<span class="small-text">({{visitPer}}개월/1회 방문)</span>' +
+                    '{{/if}}' +
+                    '{{#if visitPer == "0"}}' +
+                     '<span class="small-text">(방문없음/자가관리)</span>' +
+                    '{{/if}}' +
+                '</div>' +
+                '{{#else}}' +
+                '<div class="price-area">' +
+                '{{#if obsTotalDiscountPrice}}' +
+                    '{{#if obsBtnRule == "enable"}}'+
+                        '{{#if obsTotalDiscountPrice == 0 || obsSellingPrice == obsOriginalPrice}}' + // BTOCSITE-5387 세일가격이 값0 이였을때
+                            '{{#if obsOriginalPrice}}' +
+                            '<div class="total">' +
+                                '<em class="blind">판매가격</em>' +
+                                '<span class="price">{{obsOriginalPrice}}<em>원</em></span>' +
+                            '</div>' +
+                            '{{/if}}' +
+
+                        '{{#else}}' +
+
+                         '{{#if obsOriginalPrice}}' +
+                            '<div class="original">' +
+                                '<em class="blind">판매가격</em>' +
+                                '<span class="price">{{obsOriginalPrice}}<em>원</em></span>' +
+                            '</div>' +
+                         '{{/if}}' +
+
+                        '{{#if obsSellingPrice}}' +
+                            '<div class="total">' +
+                                '<em class="blind">총 판매가격</em>' +
+                                '<span class="price">{{obsSellingPrice}}<em>원</em></span>' +
+                            '</div>' +
+                        '{{/if}}' +
+                    '{{/if}}' +
+                '{{/if}}' +
+              '{{/if}}' +
+
+              '{{#else}}' +
+                 '{{#if obsOriginalPrice}}' +
+                    '<div class="total">' +
+                        '<em class="blind">총 판매가격</em>' +
+                        '<span class="price">{{obsOriginalPrice}}<em>원</em></span>' +
+                    '</div>' +
+                    '{{/if}}' +
+                '{{/if}}' +
+             '{{/if}}' +
+           '{{/if}}' +
+
+            '<div class="btn-area-wrap">' +
+                '<div class="btn-area">' +
+                    '<a href="{{storeUrlPath}}" class="btn border size-m btn-store">구매 매장 확인/예약하기</a>' +
+                '</div>' +
+                '<div class="btn-area">' +
+                    '<a href="{{modelUrlPath}}" class="btn border size-m btn-pdp-alert">제품 정보 확인하기</a>' +
+                '</div>' +
+            '</div>' +
+        '</div>' +
+    '</div>' +
+'</li>';
+
+var evFilter = {
+    init: function(){
+
+    },
+
+    bindEvent: function(){
+
+    },
+
+    requestData: function(){
+
+    }
+}
+
+$(function(){
+    evFilter.init();
+})
+
