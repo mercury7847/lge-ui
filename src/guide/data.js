5b5d2c3565b2c3d7d3064514e9d5b5794b302fd6
전유수 <budry@cnsparter.com>
2022-02-25 12:17:56 +0900

    LGECOMVIO-143 마이베스트샵 > 상담예약조회 퍼블(마크업) 최종

--- src/guide/data.js
+++ src/guide/data.js
@@ -387,6 +387,8 @@ var res = {
 					"배송지 주소 관리 팝업" : ["/html/MYC/ACCF7009_popup.html","","검수완료"],
 					"쿠폰" : ["/html/MYC/ACCF7018.html","","검수완료"],
 					"쿠폰 상세보기 팝업" : ["/html/MYC/ACCF7018_popup.html","","검수완료"],
+					"쿠폰(베스트샵)" : ["/html/MYC/ACCF7510.html","",""],
+					"쿠폰(베스트샵) 상세보기 팝업" : ["/html/MYC/ACCF7510_popup.html","",""],
 				},
 				"케어솔루션" : {
 					"계약현황" : ["/html/MYC/ACCF7025.html","","검수완료"],
@@ -436,9 +438,9 @@ var res = {
 				// "알림" : ["/html/MYC/ACCF7062.html","","검수완료"]
 				"마이 베스트샵" : {
 					"상담 예약 조회(방문상담/화상상담)" : ["/html/MYC/ACCF8000.html","","진행중"],
-					"상담 예약 조회(케이십 상담)" : ["/html/MYC/ACCF8010.html","","진행중"],
+					"상담 예약 조회(케어십 상담)" : ["/html/MYC/ACCF8010.html","","진행중"],
 					"상담 예약 조회(소모품 구매 예약)" : ["/html/MYC/ACCF8020.html","","진행중"],
-					"상담 예약 조회 팝업" : ["/html/MYC/ACCF8000_popup.html","","진행중"],
+					"상담 예약 조회 팝업" : ["/html/MYC/ACCF8000_popup.html","","진행중"]
 				}
 			},
 			"고객지원(CS)" : {
