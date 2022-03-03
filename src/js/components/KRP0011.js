06295c3d802626b0fac4da8c11908299098d24aa
youngnam ju <youngnam@cnspartner.com>
2022-03-03 14:07:50 +0900

    BTOCSITE-5938-567 데이터 구분자 변경

--- src/js/components/KRP0011.js
+++ src/js/components/KRP0011.js
@@ -155,7 +155,7 @@
                 });
 
                 //스펙비교하기 버튼에 모델명 교체
-                self.$compareModelIds = $compareId.join("|");
+                self.$compareModelIds = $compareId.join(","); // BTOCSITE-5938-567 데이터 구분자 변경
                 self.$section.find(".bottom-area button").attr('data-model-ids', self.$compareModelIds);
             }
         }
