401bb8086e1447b9e6b1ee3af8a69fee43c98798
yeorang.jo <yeorang.jo@cnspartner.com>
2022-03-28 17:18:28 +0900

    LGECOMVIO-23 카테고리 홈 케어용품/소모품 화면 모바일 퍼블리싱 진행

--- src/js/category-home/index.js
+++ src/js/category-home/index.js
@@ -9,13 +9,15 @@ var odc = {
        self.showRoomCarousel();
        self.showRoomMark();
 
+       self.recommendedTab();
+
        self.allView();
 
        self.bindEvent();
     },
 
     /**
-     * 이벤트 관련
+     * 공통 이벤트 관련
      */
     bindEvent: function () {
 
@@ -97,14 +99,20 @@ var odc = {
     showRoomSetting: function () {
         var $slide = $('.odc-show-room-type1__slide');
         var $indicator = $('.odc-show-room-type1__indicator');
+        var $dots = $('.odc-show-room-type1__dots');
         var $slideCurrent = $('.odc-show-room-type1__current');
         var $totalCount = $('.odc-show-room-type1__total');
         var currentIndex = $slide.siblings('.ui_carousel_current').index() || 1;
 
         if($slide.length > 1) {
             $indicator.show();
+            $dots.show();
             $slideCurrent.text(currentIndex);
             $totalCount.text($slide.length-2);
+        } else {
+            // 기존 ui 공통 carousel.js와 보여주는 기능 중 겹치는 부분이 있는데 다르게 구현해야 하여 추가함
+            $indicator.hide();
+            $dots.hide();
         }
     },
 
@@ -120,6 +128,7 @@ var odc = {
                 slidesToShow: 1,
                 slidesToScroll: 1,
                 arrows: true,
+                dotsSelector:'.ui_carousel_dots',
                 touchThreshold: 100,
                 cssEase: 'cubic-bezier(0.33, 1, 0.68, 1)',
                 speed: 150
@@ -138,6 +147,25 @@ var odc = {
             $(this).closest('.odc-show-room-type1__item').find('.odc-show-room-type1__display').removeClass('is-active');
             $(this).closest('.odc-show-room-type1__display').addClass('is-active');
         })
+    },
+
+    /**
+     * ODCC0005 : 제품 추천 탭
+     */
+    recommendedTab: function () {
+        var self = this;
+        var $tab = $('.odc-recommend-tab__button');
+        var $tabPanel = $('.odc-recommend-tab__panel');
+
+        $tab.on('click', function(){
+            var targetId = $(this).data('panelTarget');
+
+            $tab.removeClass('is-active');
+            $(this).addClass('is-active');
+
+            $tabPanel.removeClass('is-active');
+            $('#' + targetId).addClass('is-active');
+        });
     }
 }
 
